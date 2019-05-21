a7bb86f29cd8d4991bba023d71814bbdff2a070b
fulvio <fulvio.zanetti@gmail.com>
2019-05-21 13:58:35 +0200

    Setup vuex

--- src/setup/store.js
+++ src/setup/store.js
@@ -1,16 +1,37 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const initialState = {};
 
 export default new Vuex.Store({
+  modules: {},
   state: {
-
+    serverReachability: true
+  },
+  getters: {
+    getServerReachability: state => state.serverReachability
   },
   mutations: {
-
-  },
-  actions: {
-
+    /**
+     * Reset all the store
+     * 
+     * @param {state} state 
+     */
+    reset(state) {
+      Object.keys(state).forEach(key => {
+        Object.assign(state[key], initialState[key]);
+      });
+    },
+    /**
+     * Set the server reachability
+     * 
+     * @param {state} state 
+     * @param {boolean} payload 
+     */
+    setServerReachability(state, payload) {
+      state.serverReachability = payload;
+    }
   }
-})
+});
---
+++ src/store/typedefs.js
@@ -0,0 +1,3 @@
+/**
+ * Common type definitions
+ */
