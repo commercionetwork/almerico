cf1c32cd093a6c830e1bc10e9cdcec17aa1a4001
fulvio <fulvio.zanetti@gmail.com>
2019-05-21 13:52:37 +0200

    Added translations files

---
+++ src/assets/translations/index.js
@@ -0,0 +1,12 @@
+import en from "./en/index.js";
+import it from "./it/index.js";
+
+export const LOCALES = {
+  EN: "en",
+  IT: "it"
+};
+
+export default {
+  en,
+  it
+};
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "Setup/router";
 import store from "Setup/store";
+import i18n from "Setup/i18n";
 import "bootstrap";
 
 Vue.config.productionTip = false
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
