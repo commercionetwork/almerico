d966c350414f4560a79ea6520ef6baf0698322ad
fulvio <fulvio.zanetti@gmail.com>
2019-05-27 13:39:44 +0200

    Added transactions to block details

--- src/views/blocks/BlocksDetails.vue
+++ src/views/blocks/BlocksDetails.vue
@@ -45,6 +45,56 @@
           <div class="col-12">
             <h2 class="com-font-s16-w700">Transactions</h2>
           </div>
+          <div
+            v-if="transactions.length > 0"
+            class="table-responsive"
+          >
+            <table class="table table-striped">
+              <thead>
+                <tr class="text-center com-font-s12-w700">
+                  <th scope="col">TxHash</th>
+                  <th scope="col">Type</th>
+                  <th scope="col">Result</th>
+                  <th scope="col">Fee</th>
+                  <th scope="col">Height</th>
+                  <th scope="col">Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  class="text-center com-font-s12-w400"
+                  v-for="transaction in transactions.slice().reverse()"
+                  :key="transaction.id"
+                >
+                  <td class="align-middle">
+                    <router-link
+                      :to="toTransactionDetails(transaction.hash)"
+                      v-text="transaction.hash"
+                      class="d-inline-block text-truncate com-font-s10-w400"
+                      style="max-width: 120px;"
+                    />
+                  </td>
+                  <td
+                    class="align-middle"
+                    v-text="transaction.type"
+                  />
+                  <td
+                    class="align-middle"
+                    v-text="transaction.result"
+                  />
+                  <td class="align-middle">{{ transaction.fee }} ATOM</td>
+                  <td
+                    class="align-middle"
+                    v-text="transaction.height"
+                  />
+                  <td
+                    class="align-middle"
+                    v-text="getSeconds(transaction.time)"
+                  />
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
@@ -52,8 +102,11 @@
 </template>
 
 <script>
+import { ROUTE_NAMES } from "Constants";
+
 //TODO: remove
 import { mockBlock } from "Store/blocks/__mocks__/blocks";
+import { mockTransactions } from "Store/transactions/__mocks__/transactions";
 
 export default {
   name: "BlocksDetails",
@@ -66,9 +119,27 @@ export default {
       return this.$route.params.id;
     },
     title() {
-      let label = this.$t('titles.detailsForBlock');
+      let label = this.$t("titles.detailsForBlock");
       let height = this.block.height;
       return `${label} #${height}`;
+    },
+    transactions() {
+      return mockTransactions(5, this.blockHeight);
+    }
+  },
+  methods: {
+    getSeconds(time) {
+      let seconds = ((new Date() - time) / 1000).toFixed(0);
+      return `${seconds}s ago`;
+    },
+    toTransactionDetails(id) {
+      return {
+        name: ROUTE_NAMES.TRANSACTIONS_DETAILS,
+        params: {
+          lang: this.$i18n.locale,
+          id: id
+        }
+      };
     }
   }
 };
