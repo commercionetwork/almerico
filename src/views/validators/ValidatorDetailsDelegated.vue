7dcb5a395b3e71596bc76a208d8343f2781bb8ba
fulvio <fulvio.zanetti@gmail.com>
2019-05-28 12:08:19 +0200

    Added validator details delegated component

---
+++ src/views/validators/ValidatorDetailsDelegated.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-1 rounded-lg bg-light border">DELEGATED</div>
+</template>
+
+<script>
+export default {
+  name: "ValidatorDetailsDelegated",
+  description: "Display the validator delegated"
+};
+</script>
--- src/views/validators/ValidatorsDetails.vue
+++ src/views/validators/ValidatorsDetails.vue
@@ -13,11 +13,16 @@
     </div>
     <div class="py-3 px-5 rounded bg-white">
       <ValidatorDetailsHeader :validator="validator" />
+      <ValidatorDetailsDelegated
+        :validator="validator"
+        class="mt-3"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import ValidatorDetailsDelegated from "./ValidatorDetailsDelegated.vue";
 import ValidatorDetailsHeader from "./ValidatorDetailsHeader.vue";
 
 //TODO: remove
@@ -27,6 +32,7 @@ export default {
   name: "ValidatorsDetails",
   description: "Display the validator details",
   components: {
+    ValidatorDetailsDelegated,
     ValidatorDetailsHeader
   },
   computed: {
