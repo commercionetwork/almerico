78a8f27457133bfcf321bbf63acd61e49686d1ae
fulvio <fulvio.zanetti@gmail.com>
2019-06-07 15:05:04 +0200

    Added validators type definitions

--- src/store/typedefs.js
+++ src/store/typedefs.js
@@ -23,37 +23,6 @@
  * @property {Number} block 
  * @property {Number} p2p 
  */
-
-
-/** 
- * @typedef {Object} Validator 
- * @property {String} bond_height 
- * @property {Number} bond_intra_tx_counter 
- * @property {Commission} commission 
- * @property {String} consensus_pubkey 
- * @property {String} delegator_shares 
- * @property {Object} description 
- * @property {Boolean} jailed 
- * @property {String} operator_address 
- * @property {Number} status 
- * @property {String} tokens 
- * @property {String} unbonding_height 
- * @property {String} unbonding_time 
- */
-/** 
- * @typedef {Object} Commission 
- * @property {String} max_change_rate 
- * @property {String} max_rate 
- * @property {String} rate 
- * @property {String} update_time 
- */
-/** 
- * @typedef {Object} Description
- * @property {String} details 
- * @property {String} identity 
- * @property {String} moniker 
- * @property {String} website 
- */
 /** 
  * @typedef {Object} proposal
  * @property {String} description 
