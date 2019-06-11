86b7f5879bf0dc1884d2ae55e5f62ac2cdb806d1
fulvio <fulvio.zanetti@gmail.com>
2019-06-11 09:49:52 +0200

    Implemented blocks table

---
+++ src/views/blocks/TableBlocksRow.vue
@@ -0,0 +1,144 @@
+<template>
+  <tr class="text-center com-font-s12-w400">
+    <td class="align-middle">
+      <span
+        v-if="isFetching"
+        v-text="$t('messages.loading')"
+      />
+      <router-link
+        v-else
+        :to="toDetails(ROUTE_NAMES.BLOCKS_DETAILS, block.header.height)"
+        v-text="block.header.height"
+      />
+    </td>
+    <td class="align-middle">
+      <span
+        v-if="isFetching"
+        v-text="$t('messages.loading')"
+      />
+      <router-link
+        v-else
+        class="d-inline-block text-truncate"
+        style="max-width: 120px;"
+        :to="toDetails(ROUTE_NAMES.BLOCKS_DETAILS, block.header.height)"
+        v-text="block.block_id.hash"
+      />
+    </td>
+    <td class="align-middle">
+      <span
+        v-if="isFetching"
+        v-text="$t('messages.loading')"
+      />
+      <router-link
+        v-else
+        :to="toDetails(ROUTE_NAMES.VALIDATORS_DETAILS, proposerAddress)"
+        v-text="proposer"
+      />
+    </td>
+    <td class="align-middle">
+      <span
+        v-if="isFetching"
+        v-text="$t('messages.loading')"
+      />
+      <span
+        v-else
+        v-text="block.header.num_txs"
+      />
+    </td>
+    <td class="align-middle">
+      <span
+        v-if="isFetching"
+        v-text="$t('messages.loading')"
+      />
+      <span
+        v-else
+        v-text="getTime(block)"
+      />
+    </td>
+  </tr>
+</template>
+
+<script>
+import api from "Store/validators/api";
+import { PREFIX, ROUTE_NAMES } from "Constants";
+import { bech32Manager } from "Utils";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "TableBlocksRow",
+  description: "Display a row of blocks table",
+  props: {
+    block: {
+      type: Object,
+      required: true,
+      note: "Object representing a block"
+    }
+  },
+  data() {
+    return {
+      ROUTE_NAMES,
+      isFetching: false,
+      proposer: "",
+      proposerAddress: ""
+    };
+  },
+  computed: {
+    ...mapGetters("validators", {
+      validators: "allValidators"
+    })
+  },
+  methods: {
+    async getProposer() {
+      this.isFetching = true;
+      let address = bech32Manager.encode(
+        this.block.header.proposer_address,
+        PREFIX.COMNETVALCONS
+      );
+      try {
+        const response = await api.requestValidatorsetsFromHeight(
+          this.block.header.height
+        );
+        let pubKey = response.data.validators.find(x => x.address === address)
+          .pub_key;
+        let proposer = this.validators.find(x => x.consensus_pubkey === pubKey);
+        this.proposer = proposer
+          ? proposer.description.moniker
+          : "proposer name";
+        this.proposerAddress = proposer ? proposer.operator_address : "";
+      } catch (error) {
+        console.log("Get validator sets: ", error);
+      } finally {
+        this.isFetching = false;
+      }
+    },
+    getTime(block) {
+      let time = "";
+      let seconds = (new Date() - new Date(block.header.time)) / 1000;
+      switch (true) {
+        case seconds >= 3600:
+          time = `${(seconds / 3600).toFixed(0)}h`;
+          break;
+        case seconds >= 60:
+          time = `${(seconds / 60).toFixed(0)}m`;
+          break;
+        default:
+          time = `${seconds.toFixed(0)}s`;
+      }
+
+      return `${time} ago`;
+    },
+    toDetails(name, id) {
+      return {
+        name,
+        params: {
+          lang: this.$i18n.locale,
+          id
+        }
+      };
+    }
+  },
+  created() {
+    this.getProposer();
+  }
+};
+</script>
--- src/views/blocks/index.vue
+++ src/views/blocks/index.vue
@@ -8,21 +8,41 @@
       :inflation="inflation"
     />
     <div class="py-3 px-5 rounded bg-white">
-      <TableBlocks
-        :blocks="blocks"
-        :isFetching="isFetching"
+      <div
+        v-if="isFetching"
+        v-text="$t('messages.loading')"
       />
+      <div
+        v-else
+        class="table-responsive"
+      >
+        <table class="table table-striped">
+          <thead>
+            <tr class="text-center com-font-s12-w700">
+              <th scope="col">Height</th>
+              <th scope="col">Block Hash</th>
+              <th scope="col">Proposer</th>
+              <th scope="col">Txs</th>
+              <th scope="col">Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TableBlocksRow
+              v-for="block in blocks"
+              :key="block.block_id.hash"
+              :block="block"
+            />
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import SectionHeader from "Components/common/SectionHeader.vue";
-import TableBlocks from "./TableBlocks.vue";
+import TableBlocksRow from "./TableBlocksRow.vue";
 
-import api from "Store/validators/api";
-import { PREFIX } from "Constants";
-import { bech32Manager } from "Utils";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
@@ -30,13 +50,7 @@ export default {
   description: "Container for blocks' section",
   components: {
     SectionHeader,
-    TableBlocks
-  },
-  data() {
-    return {
-      blocksWithProposer: [],
-      loadingProposers: false
-    };
+    TableBlocksRow
   },
   computed: {
     ...mapGetters("blocks", {
@@ -44,10 +58,11 @@ export default {
       isFetchingBlocks: "isFetching"
     }),
     ...mapGetters("validators", {
-      validators: "allValidators"
+      validators: "allValidators",
+      isFetchingValidators: "isFetching"
     }),
     isFetching() {
-      return this.isFetchingBlocks || this.loadingProposers;
+      return this.isFetchingBlocks || this.isFetchingValidators;
     },
     price() {
       return { value: 10, iso_code: "EUR" };
@@ -68,40 +83,11 @@ export default {
     }),
     ...mapActions("validators", {
       getValidators: "getValidators"
-    }),
-    setProposers() {
-      this.loadingProposers = true;
-      let validators =
-        this.validators.length === 0 ? this.getValidators({}) : this.validators;
-
-      this.blocksWithProposer = this.blocks.map(async block => {
-        let address = bech32Manager.encode(
-          block.header.proposer_address,
-          PREFIX.COMNETVALCONS
-        );
-        try {
-          const response = await api.requestValidatorsetsFromHeight(
-            block.header.height
-          );
-          let pubKey = response.data.validators.find(x => x.address === address)
-            .pub_key;
-          let proposer = validators.find(x => x.consensus_pubkey === pubKey);
-          block.header.proposer = proposer.description.moniker;
-          block.header.proposer_address = proposer.operator_address;
-        } catch (error) {
-          console.log("Get validator sets: ", error);
-        }
-        return block;
-      });
-      this.loadingProposers = false;
-    }
+    })
   },
   created() {
-    if (this.blocks.length === 0) {
-      this.getBlocks(9);
-    } else {
-      this.setProposers();
-    }
-  }
+    this.getBlocks(9);
+    if (this.validators.length === 0) this.getValidators();
+  },
 };
 </script>
