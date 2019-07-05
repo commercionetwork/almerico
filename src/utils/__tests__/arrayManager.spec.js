8cd7676e1f611ff0f31946b3d26a955c149bef57
fulvio <fulvio.zanetti@gmail.com>
2019-07-05 16:13:46 +0200

    Added utility to group by key an objects array

--- src/utils/__tests__/arrayManager.spec.js
+++ src/utils/__tests__/arrayManager.spec.js
@@ -5,6 +5,36 @@ import {
 } from "../index";
 
 describe("utils/arrayManager", () => {
+  it("Check if the function 'groupBy' returns an objetc of objects grouped by property", () => {
+    const a = [{
+      id: 1,
+      qt: 1
+    }, {
+      id: 1,
+      qt: 2
+    }, {
+      id: 2,
+      qt: 1
+    }];
+    const expectValue = {
+      1: [{
+        id: 1,
+        qt: 1
+      }, {
+        id: 1,
+        qt: 2
+      }],
+      2: [{
+        id: 2,
+        qt: 1
+      }]
+    };
+
+    const response = arrayManager.groupBy(a, 'id');
+
+    expect(response).toEqual(expectValue);
+  });
+
   it("Check if the function 'uniqueByKey' returns an array without duplicated", () => {
     const a = [{
       id: 1
--- src/utils/arrayManager.js
+++ src/utils/arrayManager.js
@@ -1,9 +1,27 @@
 const arrayManager = {
   /**
+   * Function that groups by property an objects array
+   * 
+   * @param {Array.<Object>} objectArray 
+   * @param {String} property
+   * @returns {Object}
+   */
+  groupBy(objectArray, property) {
+    return objectArray.reduce(function (acc, obj) {
+      var key = obj[property];
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      acc[key].push(obj);
+      return acc;
+    }, {});
+  },
+  /**
    * Function that removes duplicates from an array of objects
    * 
    * @param {Array.<Object>} a 
    * @param {Function} key 
+   * @returns {Array.<Object>}
    */
   uniqueByKey(a, key) {
     return [...new Map(a.map(x => [key(x), x])).values()];
