b61bcd4dcda67d7388e04918d1ac7893a5f76dc1
fulvio <fulvio.zanetti@gmail.com>
2019-07-09 10:45:41 +0200

    Fixed search validator

--- src/views/validators/SearchValidator.vue
+++ src/views/validators/SearchValidator.vue
@@ -1,36 +1,37 @@
 <template>
-  <div class="row">
-    <div class="py-1 col-12 col-md-6">
-      <input
-        type="text"
-        class="form-control com-font-s14-w400"
-        :placeholder="$t('messages.searchValidator')"
-        v-model.lazy="moniker"
-        @change="onFilter(status)"
-        aria-label="Search"
-        aria-describedby="search-field"
-        ref="inputSearch"
-      >
+  <form @submit.prevent="onFilter(status)">
+    <div class="row">
+      <div class="py-1 col-12 col-md-6">
+        <input
+          type="text"
+          class="form-control com-font-s14-w400"
+          :placeholder="$t('messages.searchValidator')"
+          v-model="moniker"
+          aria-label="Search"
+          aria-describedby="search-field"
+          ref="inputSearch"
+        >
+      </div>
+      <div class="py-1 col-6 col-md-3">
+        <button
+          type="button"
+          class="btn btn-block px-1 com-font-s14-w700"
+          :class="[status ? 'btn-primary' : 'btn-outline-primary']"
+          v-html="$t('buttons.active')"
+          @click.stop="onFilter(true)"
+        />
+      </div>
+      <div class="py-1 col-6 col-md-3">
+        <button
+          type="button"
+          class="btn btn-block px-1 com-font-s14-w700"
+          :class="[!status ? 'btn-primary' : 'btn-outline-primary']"
+          v-html="$t('buttons.inactive')"
+          @click.stop="onFilter(false)"
+        />
+      </div>
     </div>
-    <div class="py-1 col-6 col-md-3">
-      <button
-        type="button"
-        class="btn btn-block px-1 com-font-s14-w700"
-        :class="[status ? 'btn-primary' : 'btn-outline-primary']"
-        v-html="$t('buttons.active')"
-        @click.stop="onFilter(true)"
-      />
-    </div>
-    <div class="py-1 col-6 col-md-3">
-      <button
-        type="button"
-        class="btn btn-block px-1 com-font-s14-w700"
-        :class="[!status ? 'btn-primary' : 'btn-outline-primary']"
-        v-html="$t('buttons.inactive')"
-        @click.stop="onFilter(false)"
-      />
-    </div>
-  </div>
+  </form>
 </template>
 
 <script>
