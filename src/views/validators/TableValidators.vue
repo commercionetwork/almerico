3465395f357354781bbcd50369db0971d234cf2a
fulvio <fulvio.zanetti@gmail.com>
2019-07-18 15:02:49 +0200

    Removed commented code

--- src/views/validators/TableValidators.vue
+++ src/views/validators/TableValidators.vue
@@ -45,43 +45,11 @@ export default {
     TableValidatorsRow
   },
   props: {
-    // filter: {
-    //   type: Object,
-    //   required: false,
-    //   note: "Object representing the validators filter"
-    // },
-    // pool: {
-    //   type: Object,
-    //   required: true,
-    //   note: "Object representing the pool"
-    // },
     validators: {
       type: Array,
       required: true,
       note: "The validators list to show"
     }
-  },
-  // computed: {
-  //   filteredValidators() {
-  //     const all = [...this.validators];
-  //     const bonded = parseInt(this.pool.bonded_tokens);
-  //     let cumulative = 0;
-  //     const validators = all
-  //       .sort(function(a, b) {
-  //         return b.tokens - a.tokens;
-  //       })
-  //       .map(validator => {
-  //         cumulative += validator.tokens / bonded;
-  //         validator.cumulative = cumulative;
-  //         return validator;
-  //       });
-  //     const filtered = this.filter.moniker
-  //       ? validators.filter(
-  //           validator => validator.description.moniker === this.filter.moniker
-  //         )
-  //       : validators;
-  //     return filtered;
-  //   }
-  // }
+  }
 };
 </script>
