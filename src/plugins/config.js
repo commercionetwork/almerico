9e308954a732f2c891d012edc692e2089cddec80
zenit70 <fulvio.zanetti@gmail.com>
2019-07-26 16:33:43 +0200

    Renamed config param

--- src/plugins/config.js
+++ src/plugins/config.js
@@ -1,7 +1,7 @@
 import Config from "Assets/json/config.json";
 
 export default {
-  install(Vue) {
-    Vue.prototype.$config = Config;
+  install(vue) {
+    vue.prototype.$config = Config;
   }
 };
