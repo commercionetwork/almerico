230341794ffb5486b3a5b91788c52ab52901ec87
zenit70 <fulvio.zanetti@gmail.com>
2019-09-03 14:01:56 +0200

    Added voting details votes tests

--- src/views/votings/VotingDetailsVotes.vue
+++ src/views/votings/VotingDetailsVotes.vue
@@ -31,12 +31,12 @@
           v-else-if="!isFetching && hasError"
           class="alert alert-danger"
           role="alert"
-          v-text="message"
+          v-text="$t('messages.fetchingError')"
           data-test="has-error"
         />
         <div
           v-else-if="!isFetching && !hasError && votes.length > 0"
-          data-test="item"
+          data-test="items"
         >
           <VotingDetailsVotesTable :votes="votesPage" />
         </div>
