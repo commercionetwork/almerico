c59f3ac7d0f41ceb5505330aadcae77dd218da77
zenit70 <fulvio.zanetti@gmail.com>
2019-09-26 10:57:26 +0200

    Added tx MsgSendDocument

---
+++ src/views/transactions/msgs/MsgSendDocumentReceipt.vue
@@ -0,0 +1,221 @@
+<template>
+  <MsgTx :title="title">
+    <div slot="body">
+      <div class="row p-1">
+        <div
+          class="col-12 col-md-3 com-font-s14-w700"
+          v-text="$t('labels.sender')"
+        />
+        <div class="col-12 col-md-9 text-break com-font-s14-w400">
+          <router-link
+            :to="toDetails(sender)"
+            v-text="sender"
+          />
+        </div>
+      </div>
+      <div class="row p-1">
+        <div
+          class="col-12 col-md-3 com-font-s14-w700"
+          v-text="$t('labels.recipient')"
+        />
+        <div class="col-12 col-md-9 text-break com-font-s14-w400">
+          <router-link
+            :to="toDetails(recipient)"
+            v-text="recipient"
+          />
+        </div>
+      </div>
+      <div class="row p-1">
+        <div
+          class="col-12 col-md-3 com-font-s14-w700"
+          v-text="$t('labels.uuid')"
+        />
+        <div
+          class="col-12 col-md-9 text-break com-font-s14-w400"
+          v-text="uuid"
+        />
+      </div>
+      <div class="row p-1">
+        <div
+          class="col-12 col-md-3 com-font-s14-w700"
+          v-text="$t('labels.contentUri')"
+        />
+        <div class="col-12 col-md-9 text-break com-font-s14-w400">
+          <a
+            :href="contentUri"
+            target="_blank"
+            v-text="contentUri"
+          />
+        </div>
+      </div>
+      <div class="row p-1">
+        <div
+          class="col-12 col-md-3 com-font-s14-w700"
+          v-text="$t('labels.metadata')"
+        />
+        <div class="col-12 col-md-9 text-break com-font-s14-w400">
+          <dl>
+            <dt
+              class="com-font-s14-w700"
+              v-text="$t('labels.contentUri')"
+            />
+            <dd class="com-font-s14-w400">
+              <a
+                :href="metaContentUri"
+                target="_blank"
+                v-text="metaContentUri"
+              />
+            </dd>
+            <dt
+              class="com-font-s14-w700"
+              v-text="$t('labels.schemaType')"
+            />
+            <dd
+              class="com-font-s14-w400"
+              v-text="metaSchemaType"
+            />
+            <dt
+              class="com-font-s14-w700"
+              v-text="$t('labels.schemaUri')"
+            />
+            <dd class="com-font-s14-w400">
+              <a
+                :href="metaSchemaUri"
+                target="_blank"
+                v-text="metaSchemaUri"
+              />
+            </dd>
+            <dt
+              class="com-font-s14-w700"
+              v-text="$t('labels.schemaVersion')"
+            />
+            <dd
+              class="com-font-s14-w400"
+              v-text="metaSchemaVersion"
+            />
+            <dt
+              class="com-font-s14-w700"
+              v-text="$t('labels.proof')"
+            />
+            <dd
+              class="com-font-s14-w400"
+              v-text="metaProof"
+            />
+          </dl>
+        </div>
+      </div>
+      <div class="row p-1">
+        <div
+          class="col-12 col-md-3 com-font-s14-w700"
+          v-text="$t('labels.checksum')"
+        />
+        <div class="col-12 col-md-9 text-break com-font-s14-w400">
+          <dl>
+            <dt
+              class="com-font-s14-w700"
+              v-text="$t('labels.value')"
+            />
+            <dd
+              class="com-font-s14-w400"
+              v-text="checksumValue"
+            />
+            <dt
+              class="com-font-s14-w700"
+              v-text="$t('labels.algorithm')"
+            />
+            <dd
+              class="com-font-s14-w400"
+              v-text="checksumAlgorithm"
+            />
+          </dl>
+        </div>
+      </div>
+    </div>
+  </MsgTx>
+</template>
+
+<script>
+import MsgTx from "Components/common/MsgTx.vue";
+
+import { ROUTE_NAMES } from "Constants";
+
+export default {
+  name: "MsgSendDocumentReceipt",
+  description: "Display a send document transaction message",
+  components: {
+    MsgTx
+  },
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      note: "Object representing a send document message"
+    }
+  },
+  computed: {
+    sender() {
+      return this.message.value.sender ? this.message.value.sender : "-";
+    },
+    recipient() {
+      return this.message.value.recipient ? this.message.value.recipient : "-";
+    },
+    uuid() {
+      return this.message.value.uuid ? this.message.value.uuid : "-";
+    },
+    metaContentUri() {
+      return this.message.value.metadata.content_uri
+        ? this.message.value.metadata.content_uri
+        : "-";
+    },
+    metaSchemaType() {
+      return this.message.value.metadata.schema_type
+        ? this.message.value.metadata.schema_type
+        : "-";
+    },
+    metaSchemaUri() {
+      return this.message.value.metadata.schema.uri
+        ? this.message.value.metadata.schema.uri
+        : "-";
+    },
+    metaSchemaVersion() {
+      return this.message.value.metadata.schema.version
+        ? this.message.value.metadata.schema.version
+        : "-";
+    },
+    metaProof() {
+      return this.message.value.metadata.proof
+        ? this.message.value.metadata.proof
+        : "-";
+    },
+    contentUri() {
+      return this.message.value.content_uri
+        ? this.message.value.content_uri
+        : "-";
+    },
+    checksumValue() {
+      return this.message.value.checksum.value
+        ? this.message.value.checksum.value
+        : "-";
+    },
+    checksumAlgorithm() {
+      return this.message.value.checksum.algorithm
+        ? this.message.value.checksum.algorithm
+        : "-";
+    },
+    title() {
+      return this.message.type ? this.message.type.split("/").pop() : "-";
+    }
+  },
+  methods: {
+    toDetails(id) {
+      return {
+        name: ROUTE_NAMES.ACCOUNT_DETAILS,
+        params: {
+          lang: this.$i18n.locale,
+          id
+        }
+      };
+    }
+  }
+};
+</script>
