0a31fff4277a35b44fdacffa04dc847c0c642c75
zenit70 <fulvio.zanetti@gmail.com>
2019-11-29 08:45:40 +0100

    Added buy membership tx message

---
+++ src/views/transactions/msgs/MsgBuyMembership.vue
@@ -0,0 +1,71 @@
+<template>
+  <MsgTx :title="title">
+    <div slot="body">
+      <div class="row p-1">
+        <div
+          class="col-12 col-md-3 com-font-s14-w700"
+          v-text="$t('labels.membershipType')"
+        />
+        <div
+          class="col-12 col-md-9 text-break com-font-s14-w400"
+          v-text="membershipType"
+        />
+      </div>
+      <div class="row p-1">
+        <div
+          class="col-12 col-md-3 com-font-s14-w700"
+          v-text="$t('labels.buyer')"
+        />
+        <div class="col-12 col-md-9 text-break com-font-s14-w400">
+          <router-link
+            :to="toDetails(ROUTE_NAMES.ACCOUNT_DETAILS, buyer)"
+            v-text="buyer"
+          />
+        </div>
+      </div>
+    </div>
+  </MsgTx>
+</template>
+
+<script>
+import MsgTx from "Components/common/MsgTx.vue";
+
+export default {
+  name: "MsgBuyMembership",
+  description: "Display a buy membership transaction message",
+  components: {
+    MsgTx
+  },
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      note: "Object representing a buy membership message"
+    }
+  },
+  computed: {
+    buyer() {
+      return this.message.value.buyer ? this.message.value.buyer : "-";
+    },
+    membershipType() {
+      return this.message.value.membership_type
+        ? this.message.value.membership_type
+        : "-";
+    },
+    title() {
+      return this.message.type ? this.message.type.split("/").pop() : "-";
+    }
+  },
+  methods: {
+    toDetails(name, id) {
+      return {
+        name,
+        params: {
+          lang: this.$i18n.locale,
+          id
+        }
+      };
+    }
+  }
+};
+</script>
