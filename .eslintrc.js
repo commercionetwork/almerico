96beb01ce0b5e303d09a1dcd6a2fb35202afcd48
zenit70 <fulvio.zanetti@gmail.com>
2020-03-03 11:48:22 +0100

    Updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
     'eslint:recommended'
   ],
   rules: {
-    'no-console': 'off',
-    'no-debugger': 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
