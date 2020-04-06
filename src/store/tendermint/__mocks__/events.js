adf1844e14b7288be82d09c9ba7b5c41d4a580ba
fulvio.zanetti <fulvio.zanetti@gmail.com>
2020-04-06 15:00:28 +0200

    Updated mock

--- src/store/tendermint/__mocks__/events.js
+++ src/store/tendermint/__mocks__/events.js
@@ -9,13 +9,79 @@ const mockNewBlock = (txs = null) => {
         value: {
           block: {
             header: {
-              height: 1
+              version: {
+                block: "10",
+                app: "0"
+              },
+              chain_id: "commercio-testnet7000",
+              height: "110573",
+              time: "2020-04-06T12:55:15.998788228Z",
+              last_block_id: {
+                hash: "C38147D7A7A439C64A574720E96CCC409CF16AB765BA89EC08CE58787FC3ADC2",
+                parts: {
+                  total: "1",
+                  hash: "9D7776D9C444FC640265910C2248D3E5E0077EFDB279B22A212CDFB40EF75803"
+                }
+              },
+              last_commit_hash: "E06AC4D95E17A0D5E1D595A69A8E3E24FDF356B5C67BA6642AB0DEB0883B42F6",
+              data_hash: "",
+              validators_hash: "F3FE0F9DB519DA0945168EE58248F6E0EED759E74F2895167562D25F80F549B9",
+              next_validators_hash: "F3FE0F9DB519DA0945168EE58248F6E0EED759E74F2895167562D25F80F549B9",
+              consensus_hash: "048091BC7DDC283F77BFBF91D73C44DA58C3DF8A9CBC867405D8B7F3DAADA22F",
+              app_hash: "55FBF20B1A213133B99FE9B0165FB4E8A1FA155D3F763E56542D26DD78CEBB91",
+              last_results_hash: "",
+              evidence_hash: "",
+              proposer_address: "F9742047DD71D328A1264B1FB0F63252D41852BB"
             },
             data: {
-              txs,
+              txs
             },
-            evidence: {},
-            last_commit: {}
+            evidence: {
+              evidence: null
+            },
+            last_commit: {
+              height: "110572",
+              round: "0",
+              block_id: {
+                hash: "C38147D7A7A439C64A574720E96CCC409CF16AB765BA89EC08CE58787FC3ADC2",
+                parts: {
+                  total: "1",
+                  hash: "9D7776D9C444FC640265910C2248D3E5E0077EFDB279B22A212CDFB40EF75803"
+                }
+              },
+              signatures: [
+                {
+                  block_id_flag: 2,
+                  validator_address: "09F700F953E99803FD4A134096A65E394800638D",
+                  timestamp: "2020-04-06T12:55:15.998356449Z",
+                  signature: "WmHJ5Ky0YFKUiI5JML3043OkVqYTSZg36NAUrYxIrCwqLFbwbM8UcLk3Hd7f22vM+Tvna3dY+gREY/ecPPKaCA=="
+                },
+                {
+                  block_id_flag: 2,
+                  validator_address: "485D2C4154DE89BAFCC37A49D55E67F77D19A76C",
+                  timestamp: "2020-04-06T12:55:16.000728908Z",
+                  signature: "8Hh747zZSgzbpZugpfPnSTagj7P0ClS6Z4WKiSKNfsT0Ih0cL48HxMe+n+ejEHOrswZOn+ocBOdQTA7Tb1DxBg=="
+                },
+                {
+                  block_id_flag: 2,
+                  validator_address: "C6FE3334D0146D252CF37A0660749740D48151E7",
+                  timestamp: "2020-04-06T12:55:16.003029046Z",
+                  signature: "pno5xfToL4NZMwFfNW+6KiWa6dFqTQw/wPaJL5BPSM1Y0yEE3wzqv8lVRV3V9tvhCMFK4N4UOGA+C0Z3blRkDQ=="
+                },
+                {
+                  block_id_flag: 2,
+                  validator_address: "CD03BBB06E0A3E9E42C8CBEBD9CAD2BDEC579582",
+                  timestamp: "2020-04-06T12:55:15.998788228Z",
+                  signature: "7YdpCQTNu9cc4g5CmZznTMmGfHXAN3a5XwDcUQVlniOGK5DS55kDjsyNT5cIxxgF8Qsp1LoOAQDGQ99CBAPQDQ=="
+                },
+                {
+                  block_id_flag: 2,
+                  validator_address: "F9742047DD71D328A1264B1FB0F63252D41852BB",
+                  timestamp: "2020-04-06T12:55:15.998379836Z",
+                  signature: "1Ko094eYo8FNbywaq+/0eH1vrRx//TALuVWeJJ8hsm9H/FaJ4exefXREBT7Pcj0ACsf14dq+jS5Vxb8sx+C/Dg=="
+                }
+              ]
+            }
           },
           result_begin_block: {
             events: [
@@ -50,7 +116,7 @@ const mockNewBlock = (txs = null) => {
                   },
                   {
                     key: "dmFsaWRhdG9y",
-                    value: "ZGlkOmNvbTp2YWxvcGVyMTNldzl4NzNnNjg3eDh1NGRhY3Z3OGc4NHJoMDRkdjg4ejhmcHl2"
+                    value: "ZGlkOmNvbTp2YWxvcGVyMXJrOTRua3p5eGNxN2M1dTN4NnY2dWZ0dmpndGN3bTdhM2s0OXB2"
                   }
                 ]
               },
@@ -63,7 +129,7 @@ const mockNewBlock = (txs = null) => {
                   },
                   {
                     key: "dmFsaWRhdG9y",
-                    value: "ZGlkOmNvbTp2YWxvcGVyMTNldzl4NzNnNjg3eDh1NGRhY3Z3OGc4NHJoMDRkdjg4ejhmcHl2"
+                    value: "ZGlkOmNvbTp2YWxvcGVyMXJrOTRua3p5eGNxN2M1dTN4NnY2dWZ0dmpndGN3bTdhM2s0OXB2"
                   }
                 ]
               },
@@ -76,7 +142,7 @@ const mockNewBlock = (txs = null) => {
                   },
                   {
                     key: "dmFsaWRhdG9y",
-                    value: "ZGlkOmNvbTp2YWxvcGVyMTNldzl4NzNnNjg3eDh1NGRhY3Z3OGc4NHJoMDRkdjg4ejhmcHl2"
+                    value: "ZGlkOmNvbTp2YWxvcGVyMXJrOTRua3p5eGNxN2M1dTN4NnY2dWZ0dmpndGN3bTdhM2s0OXB2"
                   }
                 ]
               },
@@ -241,7 +307,7 @@ const mockNewBlock = (txs = null) => {
                   },
                   {
                     key: "dmFsaWRhdG9y",
-                    value: "ZGlkOmNvbTp2YWxvcGVyMXh3ZDY2bTVqcjc0dTI4a3ZyZ2pzd3dydTV5MHJodmdrMGRtaDBy"
+                    value: "ZGlkOmNvbTp2YWxvcGVyMWZuMmE4ZWVqNnp4eWQzY2MzOWV4OGowZHdnNDJnbmt4bWdhamxh"
                   }
                 ]
               },
@@ -254,7 +320,7 @@ const mockNewBlock = (txs = null) => {
                   },
                   {
                     key: "dmFsaWRhdG9y",
-                    value: "ZGlkOmNvbTp2YWxvcGVyMXh3ZDY2bTVqcjc0dTI4a3ZyZ2pzd3dydTV5MHJodmdrMGRtaDBy"
+                    value: "ZGlkOmNvbTp2YWxvcGVyMWZuMmE4ZWVqNnp4eWQzY2MzOWV4OGowZHdnNDJnbmt4bWdhamxh"
                   }
                 ]
               }
@@ -266,15 +332,44 @@ const mockNewBlock = (txs = null) => {
         }
       },
       events: {
+        "transfer.recipient": [
+          "did:com:1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8s5lhfv",
+          "did:com:1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8s5lhfv"
+        ],
+        "commission.validator": [
+          "did:com:valoper1rk94nkzyxcq7c5u3x6v6uftvjgtcwm7a3k49pv",
+          "did:com:valoper13ew9x73g687x8u4dacvw8g84rh04dv88z8fpyv",
+          "did:com:valoper1p8vdenej8y3lu7d9chnfp5xlthx00exd36sgym",
+          "did:com:valoper1xwd66m5jr74u28kvrgjswwru5y0rhvgk0dmh0r",
+          "did:com:valoper1rk94nkzyxcq7c5u3x6v6uftvjgtcwm7a3k49pv",
+          "did:com:valoper1fn2a8eej6zxyd3cc39ex8j0dwg42gnkxmgajla",
+          "did:com:valoper1fn2a8eej6zxyd3cc39ex8j0dwg42gnkxmgajla"
+        ],
+        "rewards.validator": [
+          "did:com:valoper1rk94nkzyxcq7c5u3x6v6uftvjgtcwm7a3k49pv",
+          "did:com:valoper13ew9x73g687x8u4dacvw8g84rh04dv88z8fpyv",
+          "did:com:valoper1p8vdenej8y3lu7d9chnfp5xlthx00exd36sgym",
+          "did:com:valoper1xwd66m5jr74u28kvrgjswwru5y0rhvgk0dmh0r",
+          "did:com:valoper1rk94nkzyxcq7c5u3x6v6uftvjgtcwm7a3k49pv",
+          "did:com:valoper1fn2a8eej6zxyd3cc39ex8j0dwg42gnkxmgajla",
+          "did:com:valoper1fn2a8eej6zxyd3cc39ex8j0dwg42gnkxmgajla"
+        ],
+        "tm.event": [
+          "NewBlock"
+        ],
         "transfer.amount": [
           "",
           "989ucommercio"
         ],
+        "message.sender": [
+          "did:com:17xpfvakm2amg962yls6f84z3kell8c5l8jcttw",
+          "did:com:1dnmy5kgkh5yyrsvw6hsc939etm7337w66t83hw"
+        ],
         "proposer_reward.amount": [
           ""
         ],
-        "tm.event": [
-          "NewBlock"
+        "proposer_reward.validator": [
+          "did:com:valoper1rk94nkzyxcq7c5u3x6v6uftvjgtcwm7a3k49pv"
         ],
         "commission.amount": [
           "",
@@ -285,15 +380,6 @@ const mockNewBlock = (txs = null) => {
           "",
           "98.900000000000000000ucommercio"
         ],
-        "commission.validator": [
-          "did:com:valoper13ew9x73g687x8u4dacvw8g84rh04dv88z8fpyv",
-          "did:com:valoper13ew9x73g687x8u4dacvw8g84rh04dv88z8fpyv",
-          "did:com:valoper1p8vdenej8y3lu7d9chnfp5xlthx00exd36sgym",
-          "did:com:valoper1xwd66m5jr74u28kvrgjswwru5y0rhvgk0dmh0r",
-          "did:com:valoper1rk94nkzyxcq7c5u3x6v6uftvjgtcwm7a3k49pv",
-          "did:com:valoper1fn2a8eej6zxyd3cc39ex8j0dwg42gnkxmgajla",
-          "did:com:valoper1xwd66m5jr74u28kvrgjswwru5y0rhvgk0dmh0r"
-        ],
         "rewards.amount": [
           "",
           "",
@@ -302,26 +388,6 @@ const mockNewBlock = (txs = null) => {
           "",
           "",
           "989.000000000000000000ucommercio"
-        ],
-        "rewards.validator": [
-          "did:com:valoper13ew9x73g687x8u4dacvw8g84rh04dv88z8fpyv",
-          "did:com:valoper13ew9x73g687x8u4dacvw8g84rh04dv88z8fpyv",
-          "did:com:valoper1p8vdenej8y3lu7d9chnfp5xlthx00exd36sgym",
-          "did:com:valoper1xwd66m5jr74u28kvrgjswwru5y0rhvgk0dmh0r",
-          "did:com:valoper1rk94nkzyxcq7c5u3x6v6uftvjgtcwm7a3k49pv",
-          "did:com:valoper1fn2a8eej6zxyd3cc39ex8j0dwg42gnkxmgajla",
-          "did:com:valoper1xwd66m5jr74u28kvrgjswwru5y0rhvgk0dmh0r"
-        ],
-        "transfer.recipient": [
-          "did:com:1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8s5lhfv",
-          "did:com:1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8s5lhfv"
-        ],
-        "message.sender": [
-          "did:com:17xpfvakm2amg962yls6f84z3kell8c5l8jcttw",
-          "did:com:1dnmy5kgkh5yyrsvw6hsc939etm7337w66t83hw"
-        ],
-        "proposer_reward.validator": [
-          "did:com:valoper13ew9x73g687x8u4dacvw8g84rh04dv88z8fpyv"
         ]
       }
     }
