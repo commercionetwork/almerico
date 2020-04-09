5b7766d970a29d0a5c89b721dce00ab0c48e2be3
fulvio.zanetti <fulvio.zanetti@gmail.com>
2020-04-09 09:53:21 +0200

    Fixed get txs on new block event

--- src/store/tendermint/actions.js
+++ src/store/tendermint/actions.js
@@ -39,7 +39,8 @@ export default {
           root: true
         });
         if ((block.header.num_txs && parseInt(block.header.num_txs) > 0) ||
-          (block.data && block.data.txs && parseInt(block.data.txs) > 0)) {
+          (block.data && block.data.txs && block.data.txs.length > 0)) {
+
           const tag = `tx.height=${block.header.height}`;
           dispatch("transactions/fetchTransactions", tag, {
             root: true
