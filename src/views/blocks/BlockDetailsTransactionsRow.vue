38c4dc261241959873228b174c493de8592e2631
fulvio.zanetti <fulvio.zanetti@gmail.com>
2020-04-15 17:37:58 +0200

    Fixed tx status

--- src/views/blocks/BlockDetailsTransactionsRow.vue
+++ src/views/blocks/BlockDetailsTransactionsRow.vue
@@ -55,10 +55,11 @@ export default {
         : this.$t("labels.fail");
     },
     resultStatus() {
-      return this.transaction.logs
-        ? this.transaction.logs.find(log => typeof log.success !== undefined)
-            .success
-        : false;
+      return this.transaction.code ? false : true;
+      // return this.transaction.logs
+      //   ? this.transaction.logs.find(log => typeof log.success !== undefined)
+      //       .success
+      //   : false;
     },
     type() {
       if (this.transaction.tx.value.msg.length > 1)
