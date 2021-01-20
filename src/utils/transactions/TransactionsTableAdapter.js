42e52ca1e2747fd795a5a9673d9dd0bee7b5ac6e
zenit70 <fulvio.zanetti@gmail.com>
2021-01-20 16:26:14 +0100

    Improved utility

--- src/utils/transactions/TransactionsTableAdapter.js
+++ src/utils/transactions/TransactionsTableAdapter.js
@@ -32,50 +32,19 @@ class TransactionsTableAdapter {
 
   get() {
     let transactionsTable = [];
-    let transactions = [];
+    let transactions = filterTransactions(this.txs, this.filter);
 
-    if (this.filter === null || this.filter === "") {
-      transactions = this.txs.map(obj => ({
-        ...obj
-      }));
-    } else {
-      transactions = this.txs.reduce((result, transaction) => {
-        if (transaction.logs && transaction.logs.length > 0) {
-          transaction.logs.forEach(log => {
-            if (log.events && log.events.length > 0) {
-              log.events.forEach(event => {
-                if (
-                  event.type &&
-                  event.type === "message" &&
-                  event.attributes &&
-                  event.attributes.length > 0
-                ) {
-                  event.attributes.forEach(attribute => {
-                    if (attribute.value === this.filter) {
-                      result.push(transaction);
-                    }
-                  });
-                }
-              });
-            }
-          });
-        }
-
-        return result;
-      }, []);
-    }
-
-    transactions.forEach(tx => {
+    transactions.forEach((tx) => {
       const txValue = tx.tx.value;
-      const height = tx.height;
-      const type = txValue.msg.length > 1 ?
-        this.multiTypes :
-        txValue.msg.find(
-          msg => typeof msg.type !== undefined
-        ).type.split("/").pop();
-      const result = tx.code ? false : true;
+      const type =
+        txValue.msg.length > 1
+          ? this.multiTypes
+          : txValue.msg
+              .find((msg) => typeof msg.type !== undefined)
+              .type.split('/')
+              .pop();
       let amounts = [];
-      txValue.msg.forEach(msg => {
+      txValue.msg.forEach((msg) => {
         let amount = msg.value.amount;
         if (Array.isArray(amount)) {
           amounts.push.apply(amounts, amount);
@@ -83,21 +52,19 @@ class TransactionsTableAdapter {
           amounts.push(amount);
         }
       });
-      const amount = amounts.length > 0 ?
-        amounts.reduce((acc, item) => acc + parseFloat(item.amount), 0) :
-        0;
-      const fee = txValue.fee.amount.reduce((acc, item) => acc + parseFloat(item.amount), 0);
-      const hash = tx.txhash;
-      const date = new Date(tx.timestamp).toLocaleString();
+      const amount =
+        amounts.length > 0
+          ? amounts.reduce((acc, item) => acc + parseFloat(item.amount), 0)
+          : 0;
 
       const data = {
-        height: height,
+        height: tx.height,
         type: type,
-        result: result,
+        result: tx.code ? false : true,
         amount: toCoin(amount, 6, 6, this.coin),
-        fee: toCoin(fee, 6, 6, this.coin),
-        hash: hash,
-        date: date,
+        fee: formatFee(tx.tx.value, this.coin),
+        hash: tx.txhash,
+        date: new Date(tx.timestamp).toLocaleString(),
       };
       transactionsTable.push(data);
     });
@@ -107,12 +74,70 @@ class TransactionsTableAdapter {
   }
 }
 
+const formatFee = (value, coin) => {
+  const fee = value.fee.amount.reduce(
+    (acc, item) => acc + parseInt(item.amount),
+    0
+  );
+  return toCoin(fee, 6, 6, coin);
+};
+
+const filterTransactions = (transactions, filter) => {
+  if (filter === null || filter === '') {
+    return transactions.map((obj) => ({
+      ...obj,
+    }));
+  }
+  return transactions.reduce((acc, transaction) => {
+    parseTransaction(acc, transaction, filter);
+    return acc;
+  }, []);
+};
+
+const parseTransaction = (acc, transaction, filter) => {
+  if (!transaction.logs || transaction.logs.length === 0) {
+    return;
+  }
+  transaction.logs.forEach((log) => {
+    parseLog(acc, transaction, filter, log);
+  });
+};
+
+const parseLog = (acc, transaction, filter, log) => {
+  if (!log.events || log.events.length === 0) {
+    return;
+  }
+  log.events.forEach((event) => {
+    parseEvent(acc, transaction, filter, event);
+  });
+};
+
+const parseEvent = (acc, transaction, filter, event) => {
+  if (
+    !event.type ||
+    event.type !== 'message' ||
+    !event.attributes ||
+    event.attributes.length === 0
+  ) {
+    return;
+  }
+  event.attributes.forEach((attribute) => {
+    if (attribute.value === filter) {
+      acc.push(transaction);
+    }
+  });
+};
+
 const toCoin = (x, maximumFractionDigits, minimumFractionDigits, coin) => {
   const amount = x / 1000000;
-  return amount === 0 ? 0 : new Intl.NumberFormat(undefined, {
-    maximumFractionDigits,
-    minimumFractionDigits
-  }).format(amount) + " " + coin;
+  return amount === 0
+    ? 0
+    : new Intl.NumberFormat(undefined, {
+        maximumFractionDigits,
+        minimumFractionDigits,
+      }).format(amount) +
+        ' ' +
+        coin;
 };
 
 export default new TransactionsTableAdapter();
