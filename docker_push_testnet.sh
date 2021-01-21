134043c2452e18937f003180535a6b79e22c4503
Marco Ruaro <marco.ruaro@tradenet.it>
2021-01-21 12:38:43 +0100

    update ci

--- docker_push_testnet.sh
+++ docker_push_testnet.sh
@@ -2,5 +2,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_REGISTRY"
 docker tag $DOCKER_IMAGE_TESTNET_0_1_0_SCW $DOCKER_IMAGE_0_1_0_SCW:latest
 docker push $DOCKER_IMAGE_TESTNET_0_1_0_SCW
-docker push $DOCKER_IMAGE_DEVNET_0_1_0_SCW
+#docker push $DOCKER_IMAGE_DEVNET_0_1_0_SCW
 docker push $DOCKER_IMAGE_0_1_0_SCW:latest
