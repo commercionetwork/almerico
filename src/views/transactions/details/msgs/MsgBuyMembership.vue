c548da10e519711113562bb8097f14325f52fc91
zenit70 <fulvio.zanetti@gmail.com>
2021-02-01 15:08:48 +0100

    Added MsgBuyMembership

---
+++ src/views/transactions/details/msgs/MsgBuyMembership.vue
@@ -0,0 +1,69 @@
+<template>
+  <MsgTx :title="title">
+    <div slot="body">
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'membership type'"
+            />
+            {{ membershipType }}
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'buyer address'"
+            />
+            <router-link
+              :to="{
+                name: ROUTES.NAMES.VALIDATORS_ACCOUNT,
+                params: { id: buyerAddress },
+              }"
+              v-text="buyerAddress"
+            />
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+    </div>
+  </MsgTx>
+</template>
+
+<script>
+import MsgTx from '@/components/MsgTx.vue';
+
+import { ROUTES } from '@/constants';
+
+export default {
+  name: 'MsgBuyMembership',
+  description: 'Display a buy membership transaction message',
+  components: {
+    MsgTx,
+  },
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      note: 'Object representing a buy membership message',
+    },
+  },
+  data: () => ({
+    ROUTES,
+  }),
+  computed: {
+    buyerAddress() {
+      return this.message.value.buyer || '-';
+    },
+    membershipType() {
+      return this.message.value.membership_type || '-';
+    },
+    title() {
+      return this.message.type ? this.message.type.split('/').pop() : '-';
+    },
+  },
+};
+</script>
