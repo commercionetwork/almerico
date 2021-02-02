b208df2ec7bebff7bdcabc62ff28f077bfacc194
zenit70 <fulvio.zanetti@gmail.com>
2021-02-02 12:04:28 +0100

    Added MsgSendDocumentReceipt

---
+++ src/views/transactions/details/msgs/MsgSendDocumentReceipt.vue
@@ -0,0 +1,137 @@
+<template>
+  <MsgTx :title="title">
+    <div slot="body">
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-uppercase font-weight-bold"
+              v-text="'uuid'"
+            />
+            {{ uuid }}
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'sender'"
+            />
+            <router-link
+              :to="{
+                name: ROUTES.NAMES.VALIDATORS_ACCOUNT,
+                params: { id: senderAddress },
+              }"
+              v-text="senderAddress"
+            />
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'recipient'"
+            />
+            <router-link
+              :to="{
+                name: ROUTES.NAMES.VALIDATORS_ACCOUNT,
+                params: { id: recipientAddress },
+              }"
+              v-text="recipientAddress"
+            />
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'tx hash'"
+            />
+            <router-link
+              :to="{
+                name: ROUTES.NAMES.TRANSACTIONS_DETAILS,
+                params: { id: txHash },
+              }"
+              v-text="txHash"
+            />
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'document UUID'"
+            />
+            {{ documentUuid }}
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'proof'"
+            />
+            {{ proof }}
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+    </div>
+  </MsgTx>
+</template>
+
+<script>
+import MsgTx from '@/components/MsgTx.vue';
+
+import { ROUTES } from '@/constants';
+
+export default {
+  name: 'MsgSendDocumentReceipt',
+  description: 'Display a send document transaction message',
+  components: {
+    MsgTx,
+  },
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      note: 'Object representing a send document message',
+    },
+  },
+  data: () => ({
+    ROUTES,
+  }),
+  computed: {
+    documentUuid() {
+      return this.message.value.document_uuid || '-';
+    },
+    proof() {
+      return this.message.value.proof || '-';
+    },
+    recipientAddress() {
+      return this.message.value.recipient || '-';
+    },
+    senderAddress() {
+      return this.message.value.sender || '-';
+    },
+    title() {
+      return this.message.type ? this.message.type.split('/').pop() : '-';
+    },
+    txHash() {
+      return this.message.value.tx_hash || '-';
+    },
+    uuid() {
+      return this.message.value.uuid || '-';
+    },
+  },
+};
+</script>
