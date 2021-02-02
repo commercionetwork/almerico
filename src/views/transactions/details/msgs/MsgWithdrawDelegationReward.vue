b2789146fb724a78bfc38bf4a56cb5c41a60a53d
zenit70 <fulvio.zanetti@gmail.com>
2021-02-02 14:41:02 +0100

    Added MsgWithdrawDelegationReward

---
+++ src/views/transactions/details/msgs/MsgWithdrawDelegationReward.vue
@@ -0,0 +1,75 @@
+<template>
+  <MsgTx :title="title">
+    <div slot="body">
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'delegator address'"
+            />
+            <router-link
+              :to="{
+                name: ROUTES.NAMES.VALIDATORS_ACCOUNT,
+                params: { id: delegatorAddress },
+              }"
+              v-text="delegatorAddress"
+            />
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'validator address'"
+            />
+            <router-link
+              :to="{
+                name: ROUTES.NAMES.VALIDATORS_DETAILS,
+                params: { id: validatorAddress },
+              }"
+              v-text="validatorAddress"
+            />
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+    </div>
+  </MsgTx>
+</template>
+
+<script>
+import MsgTx from '@/components/MsgTx.vue';
+
+import { ROUTES } from '@/constants';
+
+export default {
+  name: 'MsgWithdrawDelegationReward',
+  description: 'Display a withdraw delegation reward transaction message',
+  components: {
+    MsgTx,
+  },
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      note: 'Object representing a withdraw delegation reward message',
+    },
+  },
+  data: () => ({
+    ROUTES,
+  }),
+  computed: {
+    delegatorAddress() {
+      return this.message.value.delegator_address || '-';
+    },
+    title() {
+      return this.message.type ? this.message.type.split('/').pop() : '-';
+    },
+    validatorAddress() {
+      return this.message.value.validator_address || '-';
+    },
+  },
+};
+</script>
