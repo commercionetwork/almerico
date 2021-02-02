0670ac807142018e5b656a59a1f12939deb5d988
zenit70 <fulvio.zanetti@gmail.com>
2021-02-02 10:25:20 +0100

    Added MsgMintCCC

---
+++ src/views/transactions/details/msgs/MsgMintCCC.vue
@@ -0,0 +1,84 @@
+<template>
+  <MsgTx :title="title">
+    <div slot="body">
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'depositor'"
+            />
+            <router-link
+              :to="{
+                name: ROUTES.NAMES.VALIDATORS_ACCOUNT,
+                params: { id: depositorAddress },
+              }"
+              v-text="depositorAddress"
+            />
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item v-for="(amount, index) in amounts" :key="index">
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'amount'"
+            />
+            {{ amount }}
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span class="pr-3 text-capitalize font-weight-bold" v-text="'id'" />
+            {{ id }}
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+    </div>
+  </MsgTx>
+</template>
+
+<script>
+import MsgTx from '@/components/MsgTx.vue';
+
+import { ROUTES } from '@/constants';
+
+export default {
+  name: 'MsgMintCCC',
+  description: 'Display a mintCCC transaction message',
+  components: {
+    MsgTx,
+  },
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      note: 'Object representing a mintCCC message',
+    },
+  },
+  data: () => ({
+    ROUTES,
+  }),
+  computed: {
+    amounts() {
+      let amounts = [];
+      for (const amount of this.message.value.deposit_amount) {
+        amounts.push(`${amount.amount} ${amount.denom}`);
+      }
+      return amounts;
+    },
+    depositorAddress() {
+      return this.message.value.depositor || '-';
+    },
+    id() {
+      return this.message.value.id || '-';
+    },
+    title() {
+      return this.message.type ? this.message.type.split('/').pop() : '-';
+    },
+  },
+};
+</script>
