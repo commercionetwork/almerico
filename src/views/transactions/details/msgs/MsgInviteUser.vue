c54b0a9dd2b13c88a96ed4e2b145e01b475fca6e
zenit70 <fulvio.zanetti@gmail.com>
2021-02-02 10:13:00 +0100

    Added MsgInviteUser

---
+++ src/views/transactions/details/msgs/MsgInviteUser.vue
@@ -0,0 +1,75 @@
+<template>
+  <MsgTx :title="title">
+    <div slot="body">
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'recipient'"
+            />
+            <router-link
+              :to="{
+                name: ROUTES.NAMES.VALIDATORS_ACCOUNT,
+                params: { id: recipient },
+              }"
+              v-text="recipient"
+            />
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <div>
+            <span
+              class="pr-3 text-capitalize font-weight-bold"
+              v-text="'sender'"
+            />
+            <router-link
+              :to="{
+                name: ROUTES.NAMES.VALIDATORS_ACCOUNT,
+                params: { id: sender },
+              }"
+              v-text="sender"
+            />
+          </div>
+        </v-list-item-content>
+      </v-list-item>
+    </div>
+  </MsgTx>
+</template>
+
+<script>
+import MsgTx from '@/components/MsgTx.vue';
+
+import { ROUTES } from '@/constants';
+
+export default {
+  name: 'MsgInviteUser',
+  description: 'Display a invite user transaction message',
+  components: {
+    MsgTx,
+  },
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      note: 'Object representing a invite user message',
+    },
+  },
+  data: () => ({
+    ROUTES,
+  }),
+  computed: {
+    recipient() {
+      return this.message.value.recipient || '-';
+    },
+    sender() {
+      return this.message.value.sender || '-';
+    },
+    title() {
+      return this.message.type ? this.message.type.split('/').pop() : '-';
+    },
+  },
+};
+</script>
