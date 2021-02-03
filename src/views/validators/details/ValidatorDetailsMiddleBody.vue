f8179d2eab5d16063069fba6d08b002110fbc33f
zenit70 <fulvio.zanetti@gmail.com>
2021-02-03 11:48:15 +0100

    Added blocks checker enabling to validator details

--- src/views/validators/details/ValidatorDetailsMiddleBody.vue
+++ src/views/validators/details/ValidatorDetailsMiddleBody.vue
@@ -1,13 +1,29 @@
 <template>
   <v-layout>
     <v-row>
-      <v-col cols="12" md="4" order="1" order-md="2">
+      <v-col
+        cols="12"
+        :md="$config.validators.isMissingBlocksChecker ? 4 : 6"
+        order="1"
+        order-md="2"
+      >
         <ValidatorDetailsDelegationsTable :delegations="delegations" />
       </v-col>
-      <v-col cols="12" md="4" order="2" order-md="3">
+      <v-col
+        cols="12"
+        :md="$config.validators.isMissingBlocksChecker ? 4 : 6"
+        order="2"
+        order-md="3"
+      >
         <ValidatorDetailsDelegatorsTable :account="account" />
       </v-col>
-      <v-col cols="12" md="4" order="3" order-md="1">
+      <v-col
+        v-if="$config.validators.isMissingBlocksChecker"
+        cols="12"
+        md="4"
+        order="3"
+        order-md="1"
+      >
         <ValidatorDetailsBlocks />
       </v-col>
     </v-row>
@@ -15,12 +31,12 @@
 </template>
 
 <script>
-import ValidatorDetailsBlocks from "./ValidatorDetailsBlocks";
-import ValidatorDetailsDelegationsTable from "./ValidatorDetailsDelegationsTable";
-import ValidatorDetailsDelegatorsTable from "./ValidatorDetailsDelegatorsTable";
+import ValidatorDetailsBlocks from './ValidatorDetailsBlocks';
+import ValidatorDetailsDelegationsTable from './ValidatorDetailsDelegationsTable';
+import ValidatorDetailsDelegatorsTable from './ValidatorDetailsDelegatorsTable';
 
 export default {
-  name: "ValidatorDetailsMiddleBody",
+  name: 'ValidatorDetailsMiddleBody',
   components: {
     ValidatorDetailsBlocks,
     ValidatorDetailsDelegationsTable,
@@ -30,12 +46,12 @@ export default {
     account: {
       type: String,
       required: true,
-      note: "The account address",
+      note: 'The account address',
     },
     delegations: {
       type: Object,
       required: true,
-      note: "The account delegations",
+      note: 'The account delegations',
     },
   },
 };
