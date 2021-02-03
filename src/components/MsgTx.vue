510d9b4973b48c79e2ed7271a5b5c53e6f92108f
zenit70 <fulvio.zanetti@gmail.com>
2021-02-03 11:20:02 +0100

    Updated layout of tx msg

--- src/components/MsgTx.vue
+++ src/components/MsgTx.vue
@@ -1,10 +1,12 @@
 <template>
   <v-card elevation="2" class="mb-3">
-    <v-list class="ma-1">
-      <v-list-item-title class="pl-1 font-weight-bold" v-text="title" />
-      <v-divider />
-      <slot name="body" />
-    </v-list>
+    <v-card-text>
+      <v-list class="ma-1">
+        <v-list-item-title class="pl-1 font-weight-bold" v-text="title" />
+        <v-divider />
+        <slot name="body" />
+      </v-list>
+    </v-card-text>
   </v-card>
 </template>
 
