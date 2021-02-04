48861eb3a49994579063606c702e75459df58470
zenit70 <fulvio.zanetti@gmail.com>
2021-02-04 15:49:12 +0100

    Updated tx layout details

--- src/views/transactions/details/TransactionDetailsSpec.vue
+++ src/views/transactions/details/TransactionDetailsSpec.vue
@@ -1,41 +1,65 @@
 <template>
   <v-card elevation="2">
-    <v-card-title class="subtitle-2 word-break" v-text="hash" />
-    <v-card-subtitle v-text="'Hash'" />
-    <v-divider />
     <v-card-text>
-      <div v-text="'Time'" />
-      <div class="pb-1 font-weight-bold" v-text="time" />
-      <div v-text="'Status'" />
-      <div
-        class="pb-1 font-weight-bold"
-        :class="statusClass"
-        v-text="statusText"
-      />
-      <div v-if="status === 0" v-text="'Raw log'" />
-      <div v-if="status === 0" class="pb-1 font-weight-bold">{{ '&ldquo;' }}{{ rawLog }}{{ '&rdquo;' }}</div>
-      <div v-text="'Fee'" />
-      <div class="pb-1 font-weight-bold" v-text="fee" />
-      <div v-text="'Gas (used/wanted)'" />
-      <div class="pb-1 font-weight-bold" v-text="gas" />
-      <div v-text="'Height'" />
-      <router-link
-        v-if="version === ''"
-        class="text-decoration-none font-weight-bold"
-        v-text="height"
-        :to="heightLink"
-      />
-      <div v-else v-text="height" />
-    </v-card-text>
-    <v-divider />
-    <v-card-text>
-      <div v-text="'Official node (raw json)'" />
-      <a
-        class="word-break"
-        :href="ledgerLink"
-        target="_blank"
-        v-text="ledgerLink"
-      />
+      <v-list class="ma-1">
+        <v-list-item-title class="word-break font-weight-bold" v-text="hash" />
+        <v-list-item-subtitle class="text-capitalize" v-text="'hash'" />
+        <v-divider />
+        <v-list-item>
+          <v-list-item-content>
+            <div class="font-weight-bold" v-text="'Time'" />
+            <div class="pb-1" v-text="time" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <div class="font-weight-bold" v-text="'Status'" />
+            <div class="pb-1" :class="statusClass" v-text="statusText" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item v-if="status === 0">
+          <v-list-item-content>
+            <div class="font-weight-bold" v-text="'Raw log'" />
+            <div class="pb-1">{{ '&ldquo;' }}{{ rawLog }}{{ '&rdquo;' }}</div>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <div class="font-weight-bold" v-text="'Fee'" />
+            <div class="pb-1" v-text="fee" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <div class="font-weight-bold" v-text="'Gas (used/wanted)'" />
+            <div class="pb-1" v-text="gas" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <div  class="font-weight-bold" v-text="'Height'" />
+            <router-link
+              v-if="version === ''"
+              class="text-decoration-none"
+              v-text="height"
+              :to="heightLink"
+            />
+            <div v-else v-text="height" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider />
+        <v-list-item>
+          <v-list-item-content>
+            <div  class="font-weight-bold" v-text="'Official node (raw json)'" />
+            <a
+              class="text-break"
+              :href="ledgerLink"
+              target="_blank"
+              v-text="ledgerLink"
+            />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-card-text>
   </v-card>
 </template>
@@ -78,6 +102,7 @@ export default {
 
 <style scoped>
 .word-break {
+  white-space: normal;
   word-break: break-all;
 }
 </style>
