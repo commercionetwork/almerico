3240b51d8bc6a956eb31af60162801a26f007016
zenit70 <fulvio.zanetti@gmail.com>
2021-02-10 14:25:24 +0100

    Improved validatordetails layout

--- src/views/validators/details/ValidatorDetailsAddress.vue
+++ src/views/validators/details/ValidatorDetailsAddress.vue
@@ -1,66 +1,64 @@
 <template>
   <v-card elevation="2">
-    <v-row>
-      <v-col cols="12" md="2">
-        <v-layout fill-height column align-center justify-center>
-          <v-img
-            v-if="picture != ''"
-            max-height="75"
-            max-width="75"
-            :src="picture"
-          />
-          <v-icon
-            v-else
-            size="50"
-            :color="details.status === 2 ? 'info' : 'warning'"
-          >
-            mdi-hubspot
-          </v-icon>
-          <span
-            class="text-caption"
-            :class="details.status === 2 ? 'info--text' : 'warning--text'"
-            v-text="status"
-          />
-        </v-layout>
-      </v-col>
-      <v-col cols="12" md="10">
-        <v-card-title class="text-capitalize" v-text="moniker" />
-        <v-card-text>
-          <div>Operator:</div>
-          <div class="font-weight-bold text-break" v-text="operator" />
-          <div>Address:</div>
-          <router-link
-            class="font-weight-bold text-decoration-none text-break"
-            v-text="account"
-            :to="toAccount"
-          />
-        </v-card-text>
-      </v-col>
-    </v-row>
+    <v-toolbar flat class="py-1">
+      <v-toolbar-title class="text-capitalize font-weight-bold">
+        {{ moniker }}
+        <div
+          class="text-caption font-weight-bold"
+          :class="details.status === 2 ? 'info--text' : 'warning--text'"
+          v-text="status"
+        />
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-img
+        v-if="picture != ''"
+        max-height="75"
+        max-width="75"
+        :src="picture"
+      />
+      <v-icon
+        v-else
+        size="50"
+        :color="details.status === 2 ? 'info' : 'warning'"
+      >
+        mdi-hubspot
+      </v-icon>
+    </v-toolbar>
+
+    <v-card-text>
+      <div>Operator:</div>
+      <div class="font-weight-bold text-break" v-text="operator" />
+      <div>Address:</div>
+      <router-link
+        class="font-weight-bold text-decoration-none text-break"
+        v-text="account"
+        :to="toAccount"
+      />
+    </v-card-text>
   </v-card>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import { ROUTES } from "@/constants";
+import { mapGetters } from 'vuex';
+import { ROUTES } from '@/constants';
 
 export default {
-  name: "ValidatorDetailsAddress",
+  name: 'ValidatorDetailsAddress',
   props: {
     account: {
       type: String,
       required: true,
-      note: "The account address",
+      note: 'The account address',
     },
     operator: {
       type: String,
       required: true,
-      note: "The operator address",
+      note: 'The operator address',
     },
   },
   computed: {
-    ...mapGetters("validators", {
-      details: "details",
+    ...mapGetters('validators', {
+      details: 'details',
     }),
     toAccount() {
       return {
@@ -69,13 +67,13 @@ export default {
       };
     },
     moniker() {
-      return this.details.description.moniker || "-";
+      return this.details.description.moniker || '-';
     },
     picture() {
-      return this.details.picture ? this.details.picture : "";
+      return this.details.picture ? this.details.picture : '';
     },
     status() {
-      return this.details.status === 2 ? "Active" : "Inactive";
+      return this.details.status === 2 ? 'active' : 'inactive';
     },
   },
 };
