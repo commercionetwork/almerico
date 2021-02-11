98fe9228aedf231166c58055a573426035545f5d
zenit70 <fulvio.zanetti@gmail.com>
2021-02-11 11:07:36 +0100

    Improved websocket event

--- src/store/starting/actions.js
+++ src/store/starting/actions.js
@@ -2,114 +2,101 @@
  * STARTING ACTIONS
  */
 
-import api from "./api";
-import {
-  STATUS,
-  WS
-} from "@/constants";
+import api from './api';
+import { STATUS, WS } from '@/constants';
 
 export default {
   /**
-   * @param {Function} dispatch 
-   * @param {Function} commit 
+   * @param {Function} dispatch
+   * @param {Function} commit
    */
-  async fetchInitData({
-    dispatch,
-    commit
-  }) {
-    commit("startLoading");
-    commit("setServerReachability", true, {
-      root: true
+  async fetchInitData({ dispatch, commit }) {
+    commit('startLoading');
+    commit('setServerReachability', true, {
+      root: true,
     });
     try {
-      await dispatch("fetchGenesis");
-      await dispatch("fetchNodeInfo");
-      await dispatch("fetchParams");
-      await dispatch("fetchPool");
-      await dispatch("blocks/fetchLatestBlock", null, {
-        root: true
+      await dispatch('fetchGenesis');
+      await dispatch('fetchNodeInfo');
+      await dispatch('fetchParams');
+      await dispatch('fetchPool');
+      await dispatch('blocks/fetchLatestBlock', null, {
+        root: true,
       });
-      await dispatch("validators/initValidators", {
-        status: [
-          STATUS.VALIDATOR.BONDED,
-          STATUS.VALIDATOR.UNBONDED,
-          STATUS.VALIDATOR.UNBONDING,
-        ],
-      }, {
-        root: true
-      })
-      dispatch("subscribeWebSocket");
+      await dispatch(
+        'validators/initValidators',
+        {
+          status: [
+            STATUS.VALIDATOR.BONDED,
+            STATUS.VALIDATOR.UNBONDED,
+            STATUS.VALIDATOR.UNBONDING,
+          ],
+        },
+        {
+          root: true,
+        }
+      );
+      dispatch('subscribeWebSocket');
     } catch (error) {
       if (error.response) {
-        commit("setError", JSON.stringify(error.response.data));
+        commit('setError', JSON.stringify(error.response.data));
       } else if (error.request) {
-        commit("setError", JSON.stringify(error));
+        commit('setError', JSON.stringify(error));
       } else {
-        commit("setServerReachability", false, {
-          root: true
+        commit('setServerReachability', false, {
+          root: true,
         });
       }
     } finally {
-      commit("stopLoading");
+      commit('stopLoading');
     }
   },
   /**
-   * @param {Function} commit 
+   * @param {Function} commit
    */
-  async fetchGenesis({
-    commit
-  }) {
+  async fetchGenesis({ commit }) {
     const response = await api.requestGenesis();
-    commit("setGenesis", response.data.result.genesis);
+    commit('setGenesis', response.data.result.genesis);
   },
   /**
-   * @param {Function} commit 
+   * @param {Function} commit
    */
-  async fetchNodeInfo({
-    commit
-  }) {
+  async fetchNodeInfo({ commit }) {
     const response = await api.requestNodeInfo();
-    commit("setNodeInfo", response.data);
+    commit('setNodeInfo', response.data);
   },
   /**
-   * @param {Function} commit 
+   * @param {Function} commit
    */
-  async fetchParams({
-    commit
-  }) {
+  async fetchParams({ commit }) {
     const response = await api.requestStakingParameters();
-    commit("setParams", response.data.result);
+    commit('setParams', response.data.result);
   },
   /**
-   * @param {Function} commit 
+   * @param {Function} commit
    */
-  async fetchPool({
-    commit
-  }) {
+  async fetchPool({ commit }) {
     const response = await api.requestPool();
-    commit("setPool", response.data.result);
+    commit('setPool', response.data.result);
   },
   /**
-   * @param {Function} dispatch 
-   * @param {Function} commit 
+   * @param {Function} dispatch
+   * @param {Function} commit
    */
-  subscribeWebSocket({
-    dispatch,
-    commit
-  }) {
+  subscribeWebSocket({ dispatch, commit }) {
     const client = new WebSocket(WS.URL);
     let connected = false;
 
-    client.onopen = function () {
+    client.onopen = function() {
       if (!connected) {
-        Object.values(WS.EVENTS).forEach(event => {
+        Object.values(WS.EVENTS).forEach((event) => {
           const msg = JSON.stringify({
-            "jsonrpc": "2.0",
-            "method": "subscribe",
-            "id": 0,
-            "params": {
-              "query": `tm.event='${event}'`
-            }
+            jsonrpc: '2.0',
+            method: 'subscribe',
+            id: 0,
+            params: {
+              query: `tm.event='${event}'`,
+            },
           });
           client.send(msg);
         });
@@ -117,35 +104,59 @@ export default {
       }
     };
 
-    client.onmessage = function (evt) {
+    client.onmessage = function(evt) {
       let eventData = JSON.parse(evt.data);
-      let type = eventData.result.data != undefined ? eventData.result.data.type.replace('tendermint/event/', '') : null;
+      let type =
+        eventData.result.data != undefined
+          ? eventData.result.data.type.replace('tendermint/event/', '')
+          : null;
       switch (type) {
         case WS.EVENTS.NEW_BLOCK:
-          commit("blocks/setLatestBlock", eventData.result.data.value.block, {
-            root: true
+          commit('blocks/setLatestBlock', eventData.result.data.value.block, {
+            root: true,
           });
           break;
         case WS.EVENTS.TX:
-          dispatch("transactions/fetchBlockTransactions", eventData.result.data.value.TxResult.height, {
-            root: true
-          })
+          dispatch(
+            'transactions/fetchBlockTransactions',
+            eventData.result.data.value.TxResult.height,
+            {
+              root: true,
+            }
+          );
+          dispatch(
+            'validators/initValidators',
+            {
+              status: [
+                STATUS.VALIDATOR.BONDED,
+                STATUS.VALIDATOR.UNBONDED,
+                STATUS.VALIDATOR.UNBONDING,
+              ],
+            },
+            {
+              root: true,
+            }
+          );
           break;
         case WS.EVENTS.VALIDATOR_SET_UPDATES:
-          commit("validators/updateValidatorsSets", eventData.result.data.value.validator_updates, {
-            root: true
-          });
+          commit(
+            'validators/updateValidatorsSets',
+            eventData.result.data.value.validator_updates,
+            {
+              root: true,
+            }
+          );
           break;
         default:
           break;
       }
     };
 
-    client.onerror = function (evt) {
-      commit("setError", JSON.stringify(evt.error));
+    client.onerror = function(evt) {
+      commit('setError', JSON.stringify(evt.error));
     };
 
-    client.onclose = function () {
+    client.onclose = function() {
       connected = false;
     };
   },
