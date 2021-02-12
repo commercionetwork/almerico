0babbd501f84458895af59fd97c9510715ba3310
zenit70 <fulvio.zanetti@gmail.com>
2021-02-12 13:20:28 +0100

    Improved action

--- src/store/blocks/actions.js
+++ src/store/blocks/actions.js
@@ -2,88 +2,74 @@
  * BLOCKS ACTIONS
  */
 
-
-import api from "./api";
+import api from './api';
 
 export default {
   /**
-   * 
+   *
    * @param {Function} commit
    * @param {Number} height
    */
-  async getBlock({
-    commit
-  }, height) {
-    commit("startLoading");
-    commit("setServerReachability", true, {
-      root: true
+  async getBlock({ commit }, height) {
+    commit('startLoading');
+    commit('setServerReachability', true, {
+      root: true,
     });
     try {
       const response = await api.requestBlock(height);
-      commit("setBlockDetails", response.data);
+      commit('setBlockDetails', response.data);
     } catch (error) {
       if (error.response) {
-        commit("setError", JSON.stringify(error.response.data));
+        commit('setError', JSON.stringify(error.response.data));
       } else if (error.request) {
-        commit("setError", JSON.stringify(error));
+        commit('setError', JSON.stringify(error));
       } else {
-        commit("setServerReachability", false, {
-          root: true
+        commit('setServerReachability', false, {
+          root: true,
         });
       }
     } finally {
-      commit("stopLoading");
+      commit('stopLoading');
     }
   },
   /**
-   * @param {Function} commit 
+   * @param {Function} commit
    */
-  async fetchLatestBlock({
-    commit
-  }) {
+  async fetchLatestBlock({ commit }) {
     const response = await api.requestLatestBlock();
-    commit("setLatestBlock", response.data.block);
+    commit('setLatestBlock', response.data.block);
   },
   /**
    * @param {Function} commit
    * @param {Number} height
    * @param {Number} items
    */
-  async fetchBlocks({
-    commit
-  }, {
-    height,
-    items
-  }) {
-    commit("startLoading");
-    commit("setServerReachability", true, {
-      root: true
+  async fetchBlocks({ commit }, { height, items }) {
+    commit('startLoading');
+    commit('setServerReachability', true, {
+      root: true,
     });
     try {
-      let maxHeight = parseInt(height);
-      const minHeight = maxHeight - items > 0 ?
-        maxHeight - items :
-        0;
-      let requests = [];
-      while (maxHeight > minHeight) {
-        requests.push(api.requestBlock(maxHeight));
-        maxHeight--;
-      }
+      const maxHeight = parseInt(height);
+      const minHeight = maxHeight - items > 0 ? maxHeight - items : 0;
+      const requests = setUpBlocksRequests(maxHeight, minHeight);
       const responses = await Promise.all(requests);
-      responses.forEach(response => commit("addSingleBlock", response.data.block));
-      commit("changeHeight", minHeight);
+      for (const response of responses) {
+        commit('addSingleBlock', response.data.block);
+      }
+      commit('changeHeight', minHeight);
     } catch (error) {
       if (error.response) {
-        commit("setError", JSON.stringify(error.response.data));
+        commit('setError', JSON.stringify(error.response.data));
       } else if (error.request) {
-        commit("setError", JSON.stringify(error));
+        commit('setError', JSON.stringify(error));
       } else {
-        commit("setServerReachability", false, {
-          root: true
+        commit('setServerReachability', false, {
+          root: true,
         });
       }
     } finally {
-      commit("stopLoading");
+      commit('stopLoading');
     }
   },
   /**
@@ -92,17 +78,11 @@ export default {
    * @param {Number} maxHeight
    * @param {Number} items
    */
-  async getBlocks({
-    commit,
-    dispatch
-  }, {
-    maxHeight,
-    items
-  }) {
-    commit("clearAllBlocks");
-    await dispatch("fetchBlocks", {
+  async getBlocks({ commit, dispatch }, { maxHeight, items }) {
+    commit('clearAllBlocks');
+    await dispatch('fetchBlocks', {
       height: maxHeight,
-      items
+      items,
     });
   },
   /**
@@ -110,15 +90,19 @@ export default {
    * @param {Number} currentHeight
    * @param {Number} items
    */
-  async addBlocks({
-    dispatch
-  }, {
-    currentHeight,
-    items
-  }) {
-    await dispatch("fetchBlocks", {
+  async addBlocks({ dispatch }, { currentHeight, items }) {
+    await dispatch('fetchBlocks', {
       height: currentHeight,
-      items
+      items,
     });
   },
+};
+
+const setUpBlocksRequests = (maxHeight, minHeight) => {
+  let requests = [];
+  while (maxHeight > minHeight) {
+    requests.push(api.requestBlock(maxHeight));
+    maxHeight--;
+  }
+  return requests;
 };
