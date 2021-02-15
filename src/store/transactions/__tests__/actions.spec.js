d87fb4ca152a0716a92e56899def040f2ee03997
zenit70 <fulvio.zanetti@gmail.com>
2021-02-15 09:12:53 +0100

    Added mocks and tests

---
+++ src/store/transactions/__tests__/actions.spec.js
@@ -0,0 +1,141 @@
+import actions from '../actions.js';
+import { mockTx, mockTxs } from '../__mocks__/txs';
+import { API } from '@/constants';
+
+const mockErrorResponse = {
+  request: {},
+  response: {
+    data: {
+      error: 'error',
+    },
+    status: 400,
+  },
+};
+const mockErrorRequestResponse = {
+  request: {},
+  response: undefined,
+};
+
+let mockError = false;
+let mockErrorRequest = false;
+let mockErrorServer = false;
+let mockResponse = null;
+
+describe('store/transactions/actions', () => {
+  beforeEach(() => {
+    mockError = false;
+    mockErrorRequest = false;
+    mockErrorServer = false;
+    mockResponse = null;
+  });
+
+  test("Check if 'actions.fetchTransaction' reset old tx details and set new tx", async () => {
+    const commit = jest.fn();
+
+    await actions.fetchTransaction({ commit }, 1);
+
+    expect(commit).toHaveBeenCalledWith('setTransactionDetails', null);
+    expect(commit).toHaveBeenCalledWith('setTransactionDetails', {
+      data: mockResponse.data,
+      ledger: API.LCD,
+      version: '',
+    });
+  });
+});
+
+jest.mock('./../api', () => ({
+  requestTransaction: () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (mockError) {
+          reject(mockErrorResponse);
+        }
+        if (mockErrorRequest) {
+          reject(mockErrorRequestResponse);
+        }
+        if (mockErrorServer) {
+          reject({});
+        }
+
+        mockResponse = {
+          data: mockTx(),
+        };
+        resolve(mockResponse);
+      }, 1);
+    });
+  },
+  requestAncestorTransaction: () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (mockError) {
+          reject(mockErrorResponse);
+        }
+        if (mockErrorRequest) {
+          reject(mockErrorRequestResponse);
+        }
+        if (mockErrorServer) {
+          reject({});
+        }
+
+        mockResponse = {
+          data: mockTx(),
+        };
+        resolve(mockResponse);
+      }, 1);
+    });
+  },
+  requestSearchTransactions: (txs = 10) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (mockError) {
+          reject(mockErrorResponse);
+        }
+        if (mockErrorRequest) {
+          reject(mockErrorRequestResponse);
+        }
+        if (mockErrorServer) {
+          reject({});
+        }
+
+        mockResponse = {
+          data: {
+            total_count: `${txs}`,
+            count: `${txs}`,
+            page_number: '1',
+            page_total: '1',
+            limit: `${txs}`,
+            txs: mockTxs(txs),
+          },
+        };
+        resolve(mockResponse);
+      }, 1);
+    });
+  },
+  requestBlockTransactions: (txs = 1) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (mockError) {
+          reject(mockErrorResponse);
+        }
+        if (mockErrorRequest) {
+          reject(mockErrorRequestResponse);
+        }
+        if (mockErrorServer) {
+          reject({});
+        }
+
+        mockResponse = {
+          data: {
+            total_count: `${txs}`,
+            count: `${txs}`,
+            page_number: '1',
+            page_total: '1',
+            limit: `${txs}`,
+            txs: mockTxs(txs),
+          },
+        };
+        resolve(mockResponse);
+      }, 1);
+    });
+  },
+}));
