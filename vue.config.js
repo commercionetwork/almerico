d9e0e8a7b3892e29fc1cdbc159b442e3ae766518
zenit70 <fulvio.zanetti@gmail.com>
2021-02-16 13:59:01 +0100

    Implemented custom title

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title =
+        process.env.VUE_APP_MAIN_TITLE !== undefined
+          ? process.env.VUE_APP_MAIN_TITLE
+          : 'Commercio Net';
+      return args;
+    });
+  },
+};
