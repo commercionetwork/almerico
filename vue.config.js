ec6784cdc69f19ce14f9f17a7d2927f32ae10813
zenit70 <fulvio.zanetti@gmail.com>
2021-02-16 14:05:27 +0100

    Updated default value

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,7 @@ module.exports = {
   transpileDependencies: ['vuetify'],
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
-      args[0].title =
-        process.env.VUE_APP_MAIN_TITLE !== undefined
-          ? process.env.VUE_APP_MAIN_TITLE
-          : 'Commercio Net';
+      args[0].title = process.env.VUE_APP_MAIN_TITLE || 'Commercio Net';
       return args;
     });
   },
