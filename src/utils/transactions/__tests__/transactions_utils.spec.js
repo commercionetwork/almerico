0062226a9b5d09ed2b1fbcb2c735d99912acae28
zenit70 <fulvio.zanetti@gmail.com>
2021-02-17 09:33:40 +0100

    Implemented tests of txs utils

--- src/utils/transactions/__tests__/transactions_utils.spec.js
+++ src/utils/transactions/__tests__/transactions_utils.spec.js
@@ -1,8 +1,10 @@
+import { mockTx, mockTxs } from '../../../store/transactions/__mocks__/txs';
 import TransactionDetailsAdapter from '../TransactionDetailsAdapter';
-import { mockTx } from '../../../store/transactions/__mocks__/txs';
+import TransactionsFilter from '../TransactionsFilter';
+import TransactionsTableAdapter from '../TransactionsTableAdapter';
 
 describe('utils/transactions', () => {
-  test("if 'TransactionDetailsAdapter' class returns the adapted transactions", () => {
+  test("if 'TransactionDetailsAdapter' class returns the adapted transaction", () => {
     const details = {
       data: mockTx(),
       ledger: 'https://lcd.commercio.network',
@@ -25,5 +27,50 @@ describe('utils/transactions', () => {
     ];
 
     expect(Object.keys(tx)).toStrictEqual(expectedKeys);
+  });
+
+  test("if 'TransactionsFilter' class filter transactions", () => {
+    const txs = mockTxs();
+    const filter = 'send';
+
+    const filterdTxs = TransactionsFilter.setTxs(txs)
+      .setFilter(filter)
+      .get();
+    for (const tx of filterdTxs) {
+      const logs = tx.logs;
+      for (const log of logs) {
+        const events = log.events;
+        for (const event of events) {
+          if (event.type === 'message') {
+            const attributes = event.attributes;
+            for (const attribute of attributes) {
+              if (attribute.key === 'action') {
+                expect(attribute.value).toBe(filter);
+              }
+            }
+          } else {
+            return;
+          }
+        }
+      }
+    }
+  });
+
+  test("if 'TransactionsTableAdapter' class returns an array of wanted transactions", () => {
+    const items = 10;
+    const txs = mockTxs(items);
+    const multiTypes = 'Multi types';
+    const filter = 'send';
+    const convertedTxs = TransactionsTableAdapter.setTxs(txs)
+      .setMultiTypes(multiTypes)
+      .setFilter(filter)
+      .get();
+
+    const expectedKeys = ['height', 'type', 'result', 'fee', 'hash', 'date'];
+
+    expect(convertedTxs.length).toBe(items);
+    for (const tx of convertedTxs) {
+      expect(Object.keys(tx)).toStrictEqual(expectedKeys);
+    }
   });
 });
