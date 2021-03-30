cd2ea9c0b93a7f4507ffe84a796d40ef01261c6d
zenit70 <fulvio.zanetti@gmail.com>
2021-03-30 10:38:58 +0200

    Added price to dashboard

--- src/views/dashboard/DashboardStats.vue
+++ src/views/dashboard/DashboardStats.vue
@@ -1,6 +1,13 @@
 <template>
   <v-card outlined>
-    <v-card-text>
+    <v-card-title class="justify-center text-h4">
+      <span class="primary--text font-weight-bold" v-text="tokenValue" />
+    </v-card-title>
+    <v-card-subtitle
+      class="text-center text-capitalize text-subtitle-2"
+      v-text="'token value'"
+    />
+    <v-card-text class="text-center">
       <span class="d-block">
         Validators:
         <span class="font-weight-bold">
@@ -12,20 +19,26 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 export default {
-  name: 'DashboardStats',
+  name: "DashboardStats",
   computed: {
-    ...mapGetters('validators', {
-      validators: 'validators',
+    ...mapGetters("starting", {
+      conversionRate: "conversionRate"
     }),
+    ...mapGetters("validators", {
+      validators: "validators"
+    }),
+    tokenValue() {
+      const rate = parseFloat(this.conversionRate);
+      return !isNaN(rate) && rate > 0 ? `€ ${(1 / rate).toFixed(2)}` : "-";
+    },
     validatorsBondeds() {
-      return this.validators.filter((validator) => validator.status === 2)
-        .length;
+      return this.validators.filter(validator => validator.status === 2).length;
     },
     validatorsTotal() {
       return this.validators.length;
-    },
-  },
+    }
+  }
 };
 </script>
