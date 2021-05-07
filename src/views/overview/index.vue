379ee03bfe3d84b3b77bd0a3a7451c273116c550
zenit70 <fulvio.zanetti@gmail.com>
2021-05-07 15:38:47 +0200

    Added page header

--- src/views/overview/index.vue
+++ src/views/overview/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    OVERVIEW
+    <v-row>
+      <v-col cols="12">
+        <OverviewHeader />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import OverviewHeader from './OverviewHeader.vue';
+
 export default {
   name: 'overview',
+  components: { OverviewHeader },
 };
 </script>
