4a619ae4a181b1c4da08ac46fe1a489b9a5b4ca7
zenit70 <fulvio.zanetti@gmail.com>
2021-09-10 14:46:45 +0200

    Add exchange rate component

---
+++ src/views/exchange-rate/ExchangeRateBodyComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-row v-if="isLoading" data-test="loading">
+    <v-col cols="12" class="pa-5">
+      <v-progress-linear
+        color="primary"
+        indeterminate
+        rounded
+        value="10"
+      ></v-progress-linear>
+    </v-col>
+  </v-row>
+  <v-row v-else-if="!isLoading && error !== null" data-test="error">
+    <v-col cols="12" class="pa-5">
+      <v-alert border="left" prominent text type="error">
+        <span class="text-body-1" v-text="errorMessage" />
+      </v-alert>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'ExchangeRateBodyComponent',
+  components: {},
+  computed: {
+    ...mapGetters('overview', {
+      error: 'error',
+      isLoading: 'isLoading',
+    }),
+    errorMessage() {
+      return this.error && this.error.data
+        ? this.error.data.error
+        : JSON.stringify(this.error);
+    },
+  },
+};
+</script>
--- src/views/exchange-rate/index.vue
+++ src/views/exchange-rate/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>EXCHANGE_RATE</div>
+  <div>
+    <ExchangeRateBodyComponent />
+    <ExchangeRateHeaderComponent />
+  </div>
 </template>
 
 <script>
+import ExchangeRateBodyComponent from './ExchangeRateBodyComponent.vue';
+import ExchangeRateHeaderComponent from './ExchangeRateHeaderComponent.vue';
+
+import { mapActions } from 'vuex';
+
 export default {
   name: 'ExchangeRate',
+  components: {
+    ExchangeRateBodyComponent,
+    ExchangeRateHeaderComponent,
+  },
+  methods: {
+    ...mapActions('overview', {
+      init: 'init',
+    }),
+  },
+  mounted() {
+    this.init();
+  },
 };
 </script>
