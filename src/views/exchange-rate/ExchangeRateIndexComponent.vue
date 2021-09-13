ce6cbc46a467c4c8cbcc4aec210513e765e9ecc1
zenit70 <fulvio.zanetti@gmail.com>
2021-09-13 15:40:24 +0200

    Add exchange rate label

--- src/views/exchange-rate/ExchangeRateIndexComponent.vue
+++ src/views/exchange-rate/ExchangeRateIndexComponent.vue
@@ -1,10 +1,7 @@
 <template>
   <v-card outlined class="pa-3">
     <v-card-text>
-      <div
-        class="text-uppercase text-h5 font-weight-black"
-        v-text="exchangeRate"
-      />
+      <div class="text-uppercase text-h5 font-weight-black" v-text="rate" />
       <div
         class="text-caption error--text"
         v-text="'(1 divided the % of Circulating Supply)'"
@@ -21,6 +18,11 @@ export default {
       type: String,
       required: true,
       note: 'The exchange rate',
+    },
+  },
+  computed: {
+    rate() {
+      return `exchange rate: ${this.exchangeRate}`;
     },
   },
 };
