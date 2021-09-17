30b3cf8fe3aae058b843ba6d510e870cba310455
zenit70 <fulvio.zanetti@gmail.com>
2021-09-17 09:38:57 +0200

    Add blocks component, move get data to first view

---
+++ src/views/exchange-rate/ExchangeRateBlockComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <TopBodyCardComponent title="exchange rate">
+    <template v-slot:content>
+      <div class="text-uppercase text-h2 font-weight-black" v-text="rate" />
+      <div
+        class="text-caption error--text"
+        v-text="'(1 divided the % of Circulating Supply)'"
+      />
+    </template>
+  </TopBodyCardComponent>
+</template>
+
+<script>
+import TopBodyCardComponent from '@/components/TopBodyCardComponent.vue';
+
+export default {
+  name: 'ExchangeRateBlockComponent',
+  components: { TopBodyCardComponent },
+  props: {
+    rate: {
+      type: String,
+      required: true,
+      note: 'The exchange rate',
+    },
+  },
+};
+</script>
---
+++ src/views/exchange-rate/ExchangeRateTopBodyComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <TopBodyComponent>
+    <template v-slot:left-info>
+      <TopBodyBlockComponent />
+    </template>
+    <template v-slot:center-info> </template>
+    <template v-slot:right-info>
+      <ExchangeRateBlockComponent :rate="rate" />
+    </template>
+  </TopBodyComponent>
+</template>
+
+<script>
+import ExchangeRateBlockComponent from './ExchangeRateBlockComponent.vue';
+import TopBodyBlockComponent from '@/components/TopBodyBlockComponent';
+import TopBodyComponent from '@/components/TopBodyComponent';
+
+export default {
+  name: 'ExchangeRateTopBodyComponent',
+  props: {
+    rate: {
+      type: String,
+      required: true,
+      note: 'The exchange rate',
+    },
+  },
+  components: {
+    ExchangeRateBlockComponent,
+    TopBodyBlockComponent,
+    TopBodyComponent,
+  },
+};
+</script>
