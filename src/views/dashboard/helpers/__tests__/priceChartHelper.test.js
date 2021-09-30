abb8681670f2cc54435dd2872566fde608925028
zenit70 <fulvio.zanetti@gmail.com>
2021-09-30 14:25:04 +0200

    Add tests

--- src/views/dashboard/helpers/__tests__/priceChartHelper.test.js
+++ src/views/dashboard/helpers/__tests__/priceChartHelper.test.js
@@ -1,11 +1,13 @@
-import { mockRateUpdates } from '../../../../store/dashboard/__mocks__/listings';
+import { RANGE } from '@/constants';
 import { dateHandler } from '@/utils';
-import priceChartHelper from '../priceChartHelper';
 import { shuffle } from 'lodash';
+import { mockRateUpdates } from '../../../../store/dashboard/__mocks__/listings';
+import priceChartHelper from '../priceChartHelper';
 
 describe('views/dashboard/helpers/priceChartHelper', () => {
+  const startingDate = dateHandler.getUtcDate('2020/2/1');
+
   test('if "getAllSortedListings" method add first rate, order items by date and return objects with "rate" and "date" keys', () => {
-    const startingDate = dateHandler.getUtcDate('2020/2/1');
     const rateUpdates = shuffle(
       mockRateUpdates({
         updates: 5,
@@ -28,6 +30,39 @@ describe('views/dashboard/helpers/priceChartHelper', () => {
     });
     all.forEach((el) => {
       expect(Object.keys(el)).toStrictEqual(['price', 'date']);
+    });
+  });
+
+  test('if "getListingsByRange" method return 2 listings when there are not updates', () => {
+    const rateUpdates = [];
+    const listings = priceChartHelper.getAllSortedListings({
+      startingDate,
+      rateUpdates,
+    });
+
+    const res = priceChartHelper.getListingsByRange({
+      listings,
+      range: RANGE.MONTH,
+    });
+
+    expect(res.length).toBe(2);
+  });
+
+  test('if "getListingsByRange" method return the wanted 2 listings when there are 1 updates', () => {
+    const rateUpdates = mockRateUpdates();
+    const listings = priceChartHelper.getAllSortedListings({
+      startingDate,
+      rateUpdates,
+    });
+
+    const res = priceChartHelper.getListingsByRange({
+      listings,
+      range: RANGE.MONTH,
+    });
+
+    expect(res.length).toBe(2);
+    res.forEach((el, i) => {
+      expect(el.price).toBe(listings[i].price);
     });
   });
 });
