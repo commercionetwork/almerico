ca6f419b2828dd909a135b3e29133addcd0d5b53
zenit70 <fulvio.zanetti@gmail.com>
2021-10-07 10:37:21 +0200

    Add account capitalization chart helper

--- src/views/validators/account/AccountDetailsCapitalizationChart.vue
+++ src/views/validators/account/AccountDetailsCapitalizationChart.vue
@@ -1,12 +1,12 @@
 <template>
-  <ChartContainerComponent :title="title">
+  <ChartContainerComponent :title="chartLabel">
     <template v-slot:chart>
       <ChartComponent
         id="account-details-capitalization-chart"
         type="bar"
         height="240"
         :dataset="chartData"
-        :options="options"
+        :options="chartOptions"
       />
     </template>
   </ChartContainerComponent>
@@ -16,7 +16,7 @@
 import ChartComponent from '@/components/chart/ChartComponent';
 import ChartContainerComponent from '@/components/chart/ChartContainerComponent';
 
-import { numberIntlFormatter } from '@/utils';
+import accountCapitalizationChartHelper from './helpers/accountCapitalizationChartHelper';
 
 export default {
   name: 'AccountDetailsCapitalizationChart',
@@ -33,71 +33,15 @@ export default {
   },
   computed: {
     chartData() {
-      return {
-        labels: ['Availables', 'Delegations', 'Unbondings', 'Rewards'],
-        datasets: [
-          {
-            data: this.datasets,
-            backgroundColor: [
-              'rgba(255, 159, 64, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 205, 86, 0.2)',
-            ],
-            borderColor: [
-              'rgb(255, 159, 64)',
-              'rgb(54, 162, 235)',
-              'rgb(153, 102, 255)',
-              'rgb(255, 205, 86)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      };
+      return accountCapitalizationChartHelper.getChartData(this.capitalization);
     },
-    options() {
-      return {
-        indexAxis: 'y',
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: false,
-          },
-          tooltip: {
-            callbacks: {
-              label: function(tooltipItem) {
-                const index = tooltipItem.dataIndex;
-                return tooltipItem.dataset.data[index].toFixed(0);
-              },
-            },
-          },
-        },
-      };
+    chartLabel() {
+      return accountCapitalizationChartHelper.getChartLabel(
+        this.capitalization,
+      );
     },
-    title() {
-      const total = this.formatTokens(this.capitalization.total);
-      return `COM Total ${total} M`;
-    },
-    datasets() {
-      return [
-        this.capitalization.availables / this.denominator,
-        this.capitalization.delegations / this.denominator,
-        this.capitalization.unbondings / this.denominator,
-        this.capitalization.rewards / this.denominator,
-      ];
-    },
-    denominator() {
-      return 1000000;
-    },
-  },
-  methods: {
-    formatTokens(value) {
-      return numberIntlFormatter.toDecimal({
-        amount: value / this.denominator,
-        maximumFractionDigits: 0,
-        minimumFractionDigits: 0,
-      });
+    chartOptions() {
+      return accountCapitalizationChartHelper.getChartOptions();
     },
   },
 };
