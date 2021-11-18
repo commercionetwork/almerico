4d9f2e2fa05349b18a5bad1d025261c27d6bfc7e
zenit70 <fulvio.zanetti@gmail.com>
2021-11-18 09:22:40 +0100

    Add MsgBuyMembership component

---
+++ src/views/transactions/detail/msgs/MsgBuyMembership.vue
@@ -0,0 +1,78 @@
+<template>
+  <TxMsgComponent :subtitle="subtitle" :title="title">
+    <div slot="body">
+      <DetailItemComponent
+        :label="$t('labels.membershipType')"
+        :content="membershipType"
+      />
+      <DetailLinkComponent
+        :label="$t('labels.buyer')"
+        :content="buyerAddress"
+        :route="{
+          name: ROUTES.NAME.ACCOUNT,
+          params: { id: buyerAddress },
+        }"
+      />
+      <DetailLinkComponent
+        :label="$t('labels.tsp')"
+        :content="tspAddress"
+        :route="{
+          name: ROUTES.NAME.ACCOUNT,
+          params: { id: tspAddress },
+        }"
+      />
+    </div>
+  </TxMsgComponent>
+</template>
+
+<script>
+import DetailItemComponent from '@/components/DetailItemComponent.vue';
+import DetailLinkComponent from '@/components/DetailLinkComponent.vue';
+import TxMsgComponent from '@/components/TxMsgComponent.vue';
+
+import { ROUTES } from '@/constants';
+import { regExpBuilder } from '@/utils';
+
+export default {
+  name: 'MsgBuyMembership',
+  description: 'Display a buy membership transaction message',
+  components: {
+    DetailItemComponent,
+    DetailLinkComponent,
+    TxMsgComponent,
+  },
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      note: 'Object representing a buy membership message',
+    },
+  },
+  data: () => ({
+    ROUTES,
+  }),
+  computed: {
+    title() {
+      const lastSegment = this.message['@type'].match(
+        regExpBuilder.getMessageTypeRegExp(),
+      )[0];
+      return lastSegment.substring(1);
+    },
+    subtitle() {
+      const firstSegments = this.message['@type'].match(
+        regExpBuilder.getMessageSourceRegExp(),
+      )[0];
+      return firstSegments.substring(1);
+    },
+    membershipType() {
+      return this.message.membership_type;
+    },
+    buyerAddress() {
+      return this.message.buyer;
+    },
+    tspAddress() {
+      return this.message.tsp;
+    },
+  },
+};
+</script>
