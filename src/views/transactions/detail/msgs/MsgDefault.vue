26b818f5c091855306e53de63279582286855f2f
zenit70 <fulvio.zanetti@gmail.com>
2022-01-13 13:01:40 +0100

    Improve default tx msg component

--- src/views/transactions/detail/msgs/MsgDefault.vue
+++ src/views/transactions/detail/msgs/MsgDefault.vue
@@ -1,27 +1,34 @@
 <template>
-  <TxMsgComponent>
-    <div slot="body">
-      <v-list-item v-for="(prop, index) in props" :key="index">
-        <DetailItemComponent
-          :label="formatKeyProp(prop[0])"
-          :content="prop[1]"
-          :isLoop="true"
-        />
-      </v-list-item>
-    </div>
-  </TxMsgComponent>
+  <v-card outlined class="mb-3">
+    <v-card-text>
+      <v-list class="text-break">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title
+              class="pb-1 font-weight-bold"
+              v-text="$t('titles.message')"
+            />
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider />
+        <v-list-item v-for="(prop, index) in props" :key="index">
+          <DetailItemComponent
+            :label="formatKeyProp(prop[0])"
+            :content="prop[1]"
+            :isLoop="true"
+          />
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 import DetailItemComponent from '@/components/DetailItemComponent.vue';
-import TxMsgComponent from '@/components/TxMsgComponent.vue';
 
 export default {
   name: 'MsgDefault',
-  components: {
-    DetailItemComponent,
-    TxMsgComponent,
-  },
+  components: { DetailItemComponent },
   props: {
     message: {
       type: Object,
