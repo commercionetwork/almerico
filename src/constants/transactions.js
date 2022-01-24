2369fc976e2d3e33453d1cf4af1c33fbb97a4559
zenit70 <fulvio.zanetti@gmail.com>
2022-01-24 09:30:50 +0100

    Tx msgs detail enable/disable

--- src/constants/transactions.js
+++ src/constants/transactions.js
@@ -1,26 +1,26 @@
 export const TRANSACTIONS = {
   TABLE_ITEMS: 30,
   SUPPORTED_TYPES: [
-    // {
-    //   name: 'MsgBeginRedelegate',
-    //   text: '/cosmos.staking.v1beta1.MsgBeginRedelegate',
-    //   value: 'begin_redelegate',
-    // },
     {
-      name: 'MsgBurnCCC',
-      text: '/commercionetwork.commercionetwork.commerciomint.MsgBurnCCC',
-      value: 'burnCCC',
+      name: 'MsgBeginRedelegate',
+      text: '/cosmos.staking.v1beta1.MsgBeginRedelegate',
+      value: 'begin_redelegate',
     },
+    // {
+    //   name: 'MsgBurnCCC',
+    //   text: '/commercionetwork.commercionetwork.commerciomint.MsgBurnCCC',
+    //   value: 'burnCCC',
+    // },
     // {
     //   name: 'MsgBuyMembership',
     //   text: '/commercionetwork.commercionetwork.commerciokyc.MsgBuyMembership',
     //   value: 'buyMembership',
     // },
-    // {
-    //   name: 'MsgCreateValidator',
-    //   text: '/cosmos.staking.v1beta1.MsgCreateValidator',
-    //   value: 'create_validator',
-    // },
+    {
+      name: 'MsgCreateValidator',
+      text: '/cosmos.staking.v1beta1.MsgCreateValidator',
+      value: 'create_validator',
+    },
     {
       name: 'MsgDelegate',
       text: '/cosmos.staking.v1beta1.MsgDelegate',
@@ -31,21 +31,21 @@ export const TRANSACTIONS = {
     //   text: '/commercionetwork.commercionetwork.commerciokyc.MsgDepositIntoLiquidityPool',
     //   value: 'depositIntoLiquidityPool',
     // },
-    // {
-    //   name: 'MsgEditValidator',
-    //   text: '/cosmos.staking.v1beta1.MsgEditValidator',
-    //   value: 'edit_validator',
-    // },
+    {
+      name: 'MsgEditValidator',
+      text: '/cosmos.staking.v1beta1.MsgEditValidator',
+      value: 'edit_validator',
+    },
     // {
     //   name: 'MsgInviteUser',
     //   text: '/commercionetwork.commercionetwork.commerciokyc.MsgInviteUser',
     //   value: 'inviteUser',
     // },
-    {
-      name: 'MsgMintCCC',
-      text: '/commercionetwork.commercionetwork.commerciomint.MsgMintCCC',
-      value: 'mintCCC',
-    },
+    // {
+    //   name: 'MsgMintCCC',
+    //   text: '/commercionetwork.commercionetwork.commerciomint.MsgMintCCC',
+    //   value: 'mintCCC',
+    // },
     {
       name: 'MsgSend',
       text: '/cosmos.bank.v1beta1.MsgSend',
@@ -56,11 +56,11 @@ export const TRANSACTIONS = {
     //   text: '/commercionetwork.commercionetwork.documents.MsgSendDocumentReceipt',
     //   value: 'sendDocumentReceipt',
     // },
-    {
-      name: 'MsgSetCCCConversionRate',
-      text: '/commercionetwork.commercionetwork.commerciomint.MsgSetCCCConversionRate',
-      value: 'setEtpsConversionRate',
-    },
+    // {
+    //   name: 'MsgSetCCCConversionRate',
+    //   text: '/commercionetwork.commercionetwork.commerciomint.MsgSetCCCConversionRate',
+    //   value: 'setEtpsConversionRate',
+    // },
     // {
     //   name: 'MsgSetCCCFreezePeriod',
     //   text: '/commercionetwork.commercionetwork.commerciomint.MsgSetCCCFreezePeriod',
@@ -71,30 +71,30 @@ export const TRANSACTIONS = {
     //   text: '/commercionetwork.commercionetwork.did.MsgSetIdentity',
     //   value: 'setIdentity',
     // },
-    {
-      name: 'MsgSetMembership',
-      text: '/commercionetwork.commercionetwork.commerciokyc.MsgSetMembership',
-      value: 'setMembership',
-    },
-    {
-      name: 'MsgShareDocument',
-      text: '/commercionetwork.commercionetwork.documents.MsgShareDocument',
-      value: 'shareDocument',
-    },
+    // {
+    //   name: 'MsgSetMembership',
+    //   text: '/commercionetwork.commercionetwork.commerciokyc.MsgSetMembership',
+    //   value: 'setMembership',
+    // },
+    // {
+    //   name: 'MsgShareDocument',
+    //   text: '/commercionetwork.commercionetwork.documents.MsgShareDocument',
+    //   value: 'shareDocument',
+    // },
     {
       name: 'MsgUndelegate',
       text: '/cosmos.staking.v1beta1.MsgUndelegate',
       value: 'begin_unbonding',
     },
-    // {
-    //   name: 'MsgUnjail',
-    //   text: '/cosmos.slashing.v1beta1.MsgUnjail',
-    //   value: 'unjail',
-    // },
-    // {
-    //   name: 'MsgWithdrawDelegatorReward',
-    //   text: '/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward',
-    //   value: 'withdraw_delegator_reward',
-    // },
+    {
+      name: 'MsgUnjail',
+      text: '/cosmos.slashing.v1beta1.MsgUnjail',
+      value: 'unjail',
+    },
+    {
+      name: 'MsgWithdrawDelegatorReward',
+      text: '/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward',
+      value: 'withdraw_delegator_reward',
+    },
   ],
 };
