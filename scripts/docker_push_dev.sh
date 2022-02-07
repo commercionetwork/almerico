3ff099759a2c59184b3c197fa5362e65625e7f17
zenit70 <fulvio.zanetti@gmail.com>
2022-02-07 13:18:50 +0100

    Add log to push script

--- scripts/docker_push_dev.sh
+++ scripts/docker_push_dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_REGISTRY"
+echo $(docker images)
 docker tag $DOCKER_IMAGE_ALMERICO_TESTNET $DOCKER_IMAGE_ALMERICO_TESTNET
 docker push $DOCKER_IMAGE_ALMERICO_TESTNET
