6812eb4a0a700a8a1500559eaf678fe5015077ad
zenit70 <fulvio.zanetti@gmail.com>
2022-04-07 14:49:24 +0200

    Fix constants

--- src/store/proposals/actions.js
+++ src/store/proposals/actions.js
@@ -1,1 +1,18 @@
-export default {};
+import { governance } from '@/apis/http';
+
+export default {
+  async initProposalsList({ commit, dispatch }) {
+    commit('reset');
+    commit('setLoading', true);
+    const requests = [];
+    await Promise.all(requests);
+    commit('setLoading', false);
+  },
+  async initProposalsDetail({ commit, dispatch }, id) {
+    commit('reset');
+    commit('setLoading', true);
+    const requests = [];
+    await Promise.all(requests);
+    commit('setLoading', false);
+  },
+};
