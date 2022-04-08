feea08c83ac7b8b328eafe1f59c97fe6bd95d218
zenit70 <fulvio.zanetti@gmail.com>
2022-04-08 12:09:32 +0200

    Add proposals icons

--- src/constants/proposals.js
+++ src/constants/proposals.js
@@ -1,10 +1,37 @@
+import {
+  mdiCurrencyUsd,
+  mdiCogOutline,
+  mdiCogOffOutline,
+  mdiTextBoxOutline,
+  mdiTune,
+} from '@mdi/js';
+
+const STATUS = {
+  UNSPECIFIED: 0,
+  DEPOSIT_PERIOD: 1,
+  VOTING_PERIOD: 2,
+  PASSED: 3,
+  REJECTED: 4,
+  FAILED: 5,
+};
+const TYPES = {
+  CANCEL_SOFTWARE_UPGRADE: 'CancelSoftwareUpgradeProposal',
+  COMMUNITY_POOL_SPEND: 'CommunityPoolSpendProposal',
+  PARAMETER_CHANGE: 'ParameterChangeProposal',
+  SOFTWARE_UPGRADE: 'SoftwareUpgradeProposal',
+  TEXT: 'TextProposal',
+};
+
+const ICONS = {
+  [TYPES.CANCEL_SOFTWARE_UPGRADE]: mdiCogOffOutline,
+  [TYPES.COMMUNITY_POOL_SPEND]: mdiCurrencyUsd,
+  [TYPES.PARAMETER_CHANGE]: mdiTune,
+  [TYPES.SOFTWARE_UPGRADE]: mdiCogOutline,
+  [TYPES.TEXT]: mdiTextBoxOutline,
+};
+
 export const PROPOSALS = {
-  STATUS: {
-    UNSPECIFIED: 0,
-    DEPOSIT_PERIOD: 1,
-    VOTING_PERIOD: 2,
-    PASSED: 3,
-    REJECTED: 4,
-    FAILED: 5,
-  },
+  ICONS,
+  STATUS,
+  TYPES,
 };
