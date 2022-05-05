ecf3933caa9a6040faefe4554ba2798b1dd89633
zenit70 <fulvio.zanetti@gmail.com>
2022-05-05 14:17:07 +0200

    Add default content component

--- src/views/proposals/detail/contents/ContentDefault.vue
+++ src/views/proposals/detail/contents/ContentDefault.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>CONTENT_DEFAULT<br />{{ content }}</div>
+  <v-list dense>
+    <v-list-item v-for="(value, key) in content" :key="key">
+      <BaseDetailItem :label="key" :content="value" :is-loop="true" />
+    </v-list-item>
+  </v-list>
 </template>
 
 <script>
+import BaseDetailItem from '@/components/BaseDetailItem';
+
 export default {
   name: 'ContentDefault',
+  components: { BaseDetailItem },
   props: {
     content: {
       type: Object,
