03aa6dd885bb3883684f65d5cc600979f6165bb1
zenit70 <fulvio.zanetti@gmail.com>
2022-06-08 10:43:32 +0200

    Remove pipeline stage

--- scripts/docker_push_v32.sh
+++
@@ -1,4 +0,0 @@
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_REGISTRY"
-echo "Publishing release $DOCKER_IMAGE_ALMERICO_V_3_2 from $TRAVIS_BRANCH branch"
-docker tag $DOCKER_IMAGE_ALMERICO_V_3_2 $DOCKER_IMAGE_ALMERICO_V_3_2
-docker push $DOCKER_IMAGE_ALMERICO_V_3_2
