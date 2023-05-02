7ff1d2f5d650a823ebc82b9ff1c40931a8be9f5e
zenit70 <fulvio.zanetti@gmail.com>
2023-05-02 13:43:12 +0200

    Update jest config

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,12 @@
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import VueRouter from 'vue-router';
 import Vuetify from 'vuetify';
+import Vuex from 'vuex';
+
+Vue.use(VueI18n);
+Vue.use(VueRouter);
 Vue.use(Vuetify);
+Vue.use(Vuex);
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
