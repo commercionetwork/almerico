7047436b62a5d855a94046227a8c41376c21943e
zenit70 <fulvio.zanetti@gmail.com>
2023-09-21 13:13:21 +0200

    Add constants & views

---
+++ src/views/assets/AssetsViewList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>AssetsViewList</div>
+</template>
+
+<script>
+export default {
+  name: 'AssetsViewList',
+};
+</script>
---
+++ src/views/assets/AssetsViewNew.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>AssetsViewNew</div>
+</template>
+
+<script>
+export default {
+  name: 'AssetsViewNew',
+};
+</script>
