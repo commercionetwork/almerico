fda3e38fcae2113ab71992e3281fda27bf2d62b8
zenit70 <fulvio.zanetti@gmail.com>
2023-10-04 13:44:33 +0200

    Add page to transfer cw20

---
+++ src/views/assets/AssetsViewDetailTransfer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>AssetsViewDetailTransfer</h1>
+    <div>{{ id }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AssetsViewDetailTransfer',
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
