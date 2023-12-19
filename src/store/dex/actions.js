765198342b2211e24fc8b08668915ea39cbf55af
zenit70 <fulvio.zanetti@gmail.com>
2023-12-19 13:30:51 +0100

    Add dex  view init

--- src/store/dex/actions.js
+++ src/store/dex/actions.js
@@ -1,7 +1,12 @@
 export default {
-  async initDex({ commit, dispatch }) {
+  async initDex({ commit, dispatch, rootGetters }) {
     commit('reset');
     commit('setLoading', true);
+    if (!rootGetters['keplr/wallet']) {
+      commit('setHasWallet', false);
+      commit('setLoading', false);
+      return;
+    }
     const requests = [];
     await Promise.all(requests);
     commit('setLoading', false);
--- src/views/dex/DexView.vue
+++ src/views/dex/DexView.vue
@@ -1,21 +1,53 @@
 <template>
-  <div>
-    <h1>DexView</h1>
-  </div>
+  <the-alert-notice
+    v-if="!hasWallet"
+    kind="warning"
+    :message="$t('msgs.noWalletConnected')"
+    data-test="alert"
+  />
+  <v-row v-else data-test="view">
+    <v-col cols="12" class="pa-5" v-if="isLoading" data-test="loading">
+      <base-loading-linear :height="25" />
+    </v-col>
+    <v-col
+      cols="12"
+      class="pa-5"
+      v-else-if="!isLoading && error"
+      data-test="error"
+    >
+      <the-error-message :error="error" />
+    </v-col>
+    <v-col cols="12" v-else data-test="content">
+      <the-header-content :title="$t('titles.dex')" />
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
   name: 'DexView',
   components: {},
-  props: {},
   data() {
     return {};
   },
-  computed: {},
-  watch: {},
-  created() {},
-  mounted() {},
-  methods: {},
+  computed: {
+    ...mapGetters('dex', ['error', 'isLoading', 'hasWallet']),
+    ...mapGetters('keplr', ['accounts']),
+  },
+  watch: {
+    accounts(newValue) {
+      if (newValue) {
+        this.initDex();
+      }
+    },
+  },
+  created() {
+    this.initDex();
+  },
+  methods: {
+    ...mapActions('dex', ['initDex']),
+  },
 };
 </script>
