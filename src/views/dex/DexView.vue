2c25b27baf7d89ca660ce1ef0c48a28b9c21ba4f
zenit70 <fulvio.zanetti@gmail.com>
2024-01-10 13:13:25 +0100

    Handle contract not available

--- src/views/dex/DexView.vue
+++ src/views/dex/DexView.vue
@@ -1,9 +1,15 @@
 <template>
   <the-alert-notice
-    v-if="!hasWallet"
+    v-if="!hasContract"
+    kind="info"
+    :message="$t('msgs.comingSoon')"
+    data-test="alert-contract"
+  />
+  <the-alert-notice
+    v-else-if="hasContract && !hasWallet"
     kind="warning"
     :message="$t('msgs.noWalletConnected')"
-    data-test="alert"
+    data-test="alert-wallet"
   />
   <v-row v-else data-test="view">
     <v-col cols="12" class="pa-5" v-if="isLoading" data-test="loading">
@@ -19,7 +25,7 @@
     </v-col>
     <v-col cols="12" v-else data-test="content">
       <the-header-content :title="$t('titles.dex')" />
-      <!-- <dex-view-form /> -->
+      <dex-view-form />
     </v-col>
   </v-row>
 </template>
@@ -39,16 +45,19 @@ export default {
   computed: {
     ...mapGetters('dex', ['error', 'isLoading', 'hasWallet']),
     ...mapGetters('keplr', ['accounts']),
+    hasContract() {
+      return !!CONTRACT.ADDRESS.DEX;
+    },
   },
   watch: {
     accounts(newValue) {
-      if (newValue && CONTRACT.ADDRESS.DEX) {
+      if (this.hasContract && newValue) {
         this.initDex(CONTRACT.ADDRESS.DEX);
       }
     },
   },
   created() {
-    if (CONTRACT.ADDRESS.DEX) {
+    if (this.hasContract) {
       this.initDex(CONTRACT.ADDRESS.DEX);
     }
   },
